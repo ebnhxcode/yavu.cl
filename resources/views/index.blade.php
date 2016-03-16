2c26ec91a7e49ba40a86c9d8a8a0608284a9e59b
Panic919 <luis.zepeda@milahorros.cl>
2016-03-16 11:08:43 -0300

    Respaldo cambios nuevos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,20 +7,8 @@
     <div class="row">
       <div class="col-md-5">
 
+        
 
-        <div id="" style="border: dashed; border-width: 2px; padding: 10px 10px 10px 10px;">
-          <div align="center"><h1>Registrate gratis!</h1></div>
-          @include('alerts.alertFields') 
-          <div>
-            {!!Form::open(['route'=>'usuarios.store', 'method'=>'POST'])!!}
-              @include('usuarios.forms.fieldsLanding')
-              <div class="form-group has-feedback has-feedback-left">
-                {!!Form::label('Registrate ya!')!!}
-                {!!Form::submit('Registrar', ['class'=>'btn btn-primary btn-success'])!!}
-              </div>
-            {!!Form::close()!!}                    
-          </div> 
-        </div>
         <br /><br />    
 
       </div>
@@ -68,10 +56,42 @@
       </div>
         <br>
         <div align="middle">
-        <img src="img/cards/registrate_01a.png" class="img-responsive" >
+        <a data-toggle="modal" data-target="#gridSystemModal" role="button" href="#!"><img src="img/cards/registrate_01a.png" class="img-responsive" ></a>
         <br>
         <img src="img/cards/barra_separadora.png" class="img-responsive">
         </div>
+
+
+
+
+        <div id="gridSystemModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="gridSystemModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+
+      <div id="" style="border: dashed; border-width: 2px; padding: 10px 10px 10px 10px;">
+          <div align="center"><h1>Registrate gratis!</h1></div>
+          @include('alerts.alertFields') 
+          <div>
+            {!!Form::open(['route'=>'usuarios.store', 'method'=>'POST'])!!}
+              @include('usuarios.forms.fieldsLanding')
+              <div class="form-group has-feedback has-feedback-left">
+
+                {!!Form::submit('Registrar', ['class'=>'btn btn-primary btn-success'])!!}
+              </div>
+            {!!Form::close()!!}                    
+          </div> 
+        </div>
+
+
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 
     </div>
 
