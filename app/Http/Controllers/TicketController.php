e7370eade755f0d6a9f4efce27239db64d84b853
root <root@localhost.localdomain>
2016-04-01 21:01:59 -0300

    Optimizacion user

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -12,92 +12,85 @@ use Auth;
 use Redirect;
 use Illuminate\Routing\Route;
 use DB;
-class TicketController extends Controller
-{
-    public function __construct(){
-
-        $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
+class TicketController extends Controller{
+  public function __construct(){
+    $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
+  }
+  public function create(){
+    return view('tickets.create');
+  }
+  public function ContarTickets(){
+    if(isset(Auth::user()->get()->id)){
+      $tickets = DB::table('tickets')
+        ->select(DB::raw('sum(cantidad_tickets) as tickets'))
+        ->where('user_id', '=', Auth::user()->get()->id)
+        ->groupBy('user_id')
+        ->get();
+      return response()->json(
+        $tickets
+      );
+    }else{
+      return response()->json(
+        'Registrate o inicia sesión'
+      );
     }
-    public function find(Route $route){
-        $this->ticket = Ticket::find($route->getParameter('tickets'));
-    }    
-    public function index()
-    {
-        $tickets = Ticket::paginate(5);
-        return view('tickets.index', compact('tickets'));
-    }
-    public function create()
-    {
-        return view('tickets.create');
-    }
-
-    public function ContarTickets()
-    {
-        $tickets = DB::table('tickets')
-                    ->select(DB::raw('sum(cantidad_tickets) as tickets'))
-                    ->where('user_id', '=', Auth::user()->get()->id)   
-                    ->groupBy('user_id')
-                    //->orderBy('created_at','desc')   
-                    ->get();        
-        return response()->json(
-            $tickets
-        );        
-    }
-
-    public function EfectuarCompra($user_id, $cantidadtickets)
-    {
-        $coinsUsuario = DB::table('registro_coins')
-                ->where('user_id', $user_id)
-                ->sum('cantidad');    
-
-        $valorCompra = (int) $cantidadtickets*100;
-
-        if((int) $coinsUsuario >= (int) $valorCompra ){            
-            DB::table('registro_coins')->insert(
-                ['user_id' => $user_id, 
-                'motivo' => 'Compra de ticket'.(($cantidadtickets>1)?'s':''),
-                'cantidad' => $valorCompra*-1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()                
-                ]
-            );   
-            DB::table('tickets')->insert(
-                ['user_id' => $user_id, 
-                'cantidad_tickets' => $cantidadtickets, 
-                'monto' => ((int) $cantidadtickets * 100),
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-                ]
-            );                 
-            return response()->json(
-               'Exito'
-            );                       
-
-        }else{
-            return response()->json(
-               'Sin saldo para el servicio'
-            );                      
-        }
-        return true;
-    }
-  
-    public function store(TicketCreateRequest $request)
-    {
-        Ticket::create($request->all());
-        Session::flash('message', 'Ticket creado correctamente');
-        return Redirect::to('/tickets');
-    }
-    public function show($id)
-    {
-
-    }   
-    public function VerificarTickets($user_id)
-    {
-        $ticketsUsuario = DB::table('tickets')
-                ->where('user_id', $user_id)
-                ->sum('cantidad_tickets');           
-        return response()->json(
-            $ticketsUsuario
+  }
+  public function EfectuarCompra($user_id, $cantidadtickets){
+    if(isset($user_id) && isset($cantidadtickets)){
+      $coinsUsuario = DB::table('registro_coins')
+        ->where('user_id', $user_id)
+        ->sum('cantidad');
+      $valorCompra = (int) $cantidadtickets*100;
+      if((int) $coinsUsuario >= (int) $valorCompra ){
+        DB::table('registro_coins')->insert(
+          ['user_id' => $user_id,
+            'motivo' => 'Compra de ticket'.(($cantidadtickets>1)?'s':''),
+            'cantidad' => $valorCompra*-1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+          ]
         );
+        DB::table('tickets')->insert(
+          ['user_id' => $user_id,
+            'cantidad_tickets' => $cantidadtickets,
+            'monto' => ((int) $cantidadtickets * 100),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+          ]
+        );
+        return response()->json(
+          'Exito'
+        );
+      }else{
+        return response()->json(
+          'Sin saldo para el servicio'
+        );
+      }
+    }
+    return response()->json(
+      'Acceso inconrrecto'
+    );
+  }
+  public function find(Route $route){
+    $this->ticket = Ticket::find($route->getParameter('tickets'));
+  }
+  public function index(){
+    $tickets = Ticket::paginate(5);
+    return view('tickets.index', compact('tickets'));
+  }
+  public function show($id){
+  }
+  public function store(TicketCreateRequest $request){
+      Ticket::create($request->all());
+      Session::flash('message', 'Ticket creado correctamente');
+      return Redirect::to('/tickets');
+    }
+  public function VerificarTickets($user_id){
+      $ticketsUsuario = DB::table('tickets')
+        ->where('user_id', $user_id)
+        ->sum('cantidad_tickets');
+      return response()->json(
+        $ticketsUsuario
+      );
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,186 +19,163 @@ use Malahierba\ChileRut\ChileRut;
 class UserController extends Controller
 {
   public function __construct(){
-
     $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
   }
   public function find(Route $route){
     $this->user = User::find($route->getParameter('usuarios'));
     //return $this->user;
   }
-  public function index()
-  {
+  public function BuscarUsuarios($nombre){
+    $nombre = addslashes($nombre);
+    if(isset($nombre)){
+      $usuarios = DB::table('users')
+        ->select('*')
+        ->where('nombre', 'like', '%'.$nombre.'%')
+        ->orwhere('apellido', 'like', '%'.$nombre.'%')
+        ->orwhere('email', 'like', '%'.$nombre.'%')
+        ->orwhere('ciudad', 'like', '%'.$nombre.'%')
+        ->orwhere('region', 'like', '%'.$nombre.'%')
+        ->orwhere('pais', 'like', '%'.$nombre.'%')
+        ->orderBy('created_at','desc')
+        ->get();
+      return response()->json(
+        $usuarios
+      );
+    }
+    return response()->json("No se encontró la búsqueda.");
+  }
+  public function create(){
+    return view('usuarios.create');
+  }
+  public function dashboard(){
+    $users = DB::table('users')
+      ->select('*')
+      ->where('id', '=', Auth::user()->get()->id)
+      ->get();
+    return view('usuarios.dashboard', compact('users'));
+  }
+  public function destroy($id){
+    if(isset($id)){
+      $this->user->delete();
+      Session::flash('message', 'Usuario eliminado correctamente');
+      return Redirect::to('/usuarios');
+    }
+    return response()->json(
+      'No se encontró el usuario'
+    );
+  }
+  public function edit($id){
+    return view('usuarios.edit', ['user' => $this->user]);
+  }
+  public function getCodigoVerificacion(){
+    $codigo = Carbon::now()->second.
+      Carbon::now()->minute.
+      Carbon::now()->hour."V";
+    return $codigo;
+  }
+  public function index(){
     $users = User::paginate(10);
     //$users = User::onlyTrashed()->paginate(5);
     return view('usuarios.index', compact('users'));
   }
-  public function BuscarUsuarios($nombre){
-    $nombre = addslashes($nombre);
-    $usuarios = DB::table('users')                    
-                ->select('*')
-                ->where('nombre', 'like', '%'.$nombre.'%')
-                ->orwhere('apellido', 'like', '%'.$nombre.'%')
-                ->orwhere('email', 'like', '%'.$nombre.'%')
-                ->orwhere('ciudad', 'like', '%'.$nombre.'%')
-                ->orwhere('region', 'like', '%'.$nombre.'%')
-                ->orwhere('pais', 'like', '%'.$nombre.'%')
-                ->orderBy('created_at','desc')
-                ->get();
-
-    //dd($usuarios);
+  public function InfoEmpresas($user_id){
+    if(isset($user_id)){
+      $info = DB::table('empresas')
+        ->select('*')
+        ->where('user_id', '=', $user_id)
+        ->orderBy('created_at','desc')
+        ->get();
+      return response()->json(
+        $info
+      );
+    }
     return response()->json(
-        $usuarios
+      'No se encontró la empresa'
     );
   }
-  public function create()
-  {
-    return view('usuarios.create');
+  public function profile(){
+    return view('usuarios.profile');
   }
-  public function store(UserCreateRequest $request)
-  {
+  public function show($id){
+
+  }
+  public function store(UserCreateRequest $request){
     //User::create($request->all());
     $existeReferente = User::where('referente', $request->referido)->first();
-
     if ($existeReferente){
-      
       $CodigoVerificacion = $this->getCodigoVerificacion();
-      
       User::create([
-      'nombre' => $request->nombre,
-      'apellido' => $request->apellido,
-      'email' => $request->email,
-      'password' => $request->password,
-      'estado' => 'inactivo',
-      'referido' => $request ->referido,
-      'referente' => Carbon::now()->second.Carbon::now()->minute.Carbon::now()->hour.Carbon::now()->year.Carbon::now()->month.Carbon::now()->day."RY",
-      'validacion' => $CodigoVerificacion,
-      'ciudad' => $request->ciudad
-      ])->first()->save();        
- 
-
-      
+        "nombre"    => $request->nombre,
+        "apellido"  => $request->apellido,
+        "email"     => $request->email,
+        "password"  => $request->password,
+        "estado"    => "inactivo",
+        "referido"  => $request ->referido,
+        "referente" =>  Carbon::now()->second.
+          Carbon::now()->minute.
+          Carbon::now()->hour.
+          Carbon::now()->year.
+          Carbon::now()->month.
+          Carbon::now()->day."RY",
+        "validacion"=> $CodigoVerificacion,
+        "ciudad"    => $request->ciudad,
+      ])->first()->save();
       Mail::send('emails.register', ['email' => \Input::get('email'), 'nombre' => \Input::get('nombre'), 'codigo' => $CodigoVerificacion ], function($msj){
-
-          $msj->subject('Correo de Contacto');
-
-          $msj->to(\Input::get('email'));
-
-      }); 
-      
-
+        $msj->subject('Correo de Contacto');
+        $msj->to(\Input::get('email'));
+        return true;
+      });
       $usuario = User::where('email', \Input::get('email'))->first();
-
-      //echo $usuario->id;
-
       if($usuario){
-
         DB::table('registro_coins')->insert(
-            ['user_id' => $usuario->id, 
-            'cantidad' => '70', 
-            'motivo'   => 'Uso de código referido',
-            'created_at' => strftime( "%Y-%m-%d-%H-%M-%S", time()),
-            'updated_at' => strftime( "%Y-%m-%d-%H-%M-%S", time())]
+          ['user_id'    => $usuario->id,
+            'cantidad'    => '70',
+            'motivo'      => 'Uso de código referido',
+            'created_at'  => strftime( "%Y-%m-%d-%H-%M-%S", time()),
+            'updated_at'  => strftime( "%Y-%m-%d-%H-%M-%S", time())]
         );
-
         DB::table('pops')->insert(
-                  ['user_id' => $usuario->id, 
-                  'empresa_id' => 1,
-                  'tipo' => 'referido', 
-                  'estado'   => 'pendiente',
-                  'contenido' => 'Se cargaron coins por código de referido! ',
-                  'created_at' => strftime( "%Y-%m-%d-%H-%M-%S", time()),
-                  'updated_at' => strftime( "%Y-%m-%d-%H-%M-%S", time())]
-              );    
-
+          ['user_id'    => $usuario->id,
+            'empresa_id'  => 1,
+            'tipo'        => 'referido',
+            'estado'      => 'pendiente',
+            'contenido'   => 'Se cargaron coins por código de referido! ',
+            'created_at'  => strftime( "%Y-%m-%d-%H-%M-%S", time()),
+            'updated_at'  => strftime( "%Y-%m-%d-%H-%M-%S", time())]
+        );
         Session::flash('message', 'Usuario creado correctamente');
-        return Redirect::to('/');        
+        return Redirect::to('/');
       }
-
-
-
-
-      
-    
     }else{
       //CUANDO NO EXISTE REFERENTE
-        $CodigoVerificacion = $this->getCodigoVerificacion();
-        Mail::send('emails.register', ['email' => \Input::get('email'), 'nombre' => \Input::get('nombre'), 'codigo' => $CodigoVerificacion ], function($msj){
-
-            $msj->subject('Correo de Contacto');
-
-            $msj->to(\Input::get('email'));
-
-        }); 
-        
-      
-       User::create([
-      'nombre' => $request->nombre,
-      'apellido' => $request->apellido,
-      'email' => $request->email,
-      'password' => $request->password,
-      'estado' => 'inactivo',
-      'referido' => '',
-      'referente' => Carbon::now()->second.Carbon::now()->minute.Carbon::now()->hour.Carbon::now()->year.Carbon::now()->month.Carbon::now()->day."RY",
-      'validacion' => $CodigoVerificacion,
-      'ciudad' => $request->ciudad
+      $CodigoVerificacion = $this->getCodigoVerificacion();
+      Mail::send('emails.register', ['email' => \Input::get('email'), 'nombre' => \Input::get('nombre'), 'codigo' => $CodigoVerificacion ], function($msj){
+        $msj->subject('Correo de Contacto');
+        $msj->to(\Input::get('email'));
+      });
+      User::create([
+        "nombre"      =>  $request->nombre,
+        "apellido"    =>  $request->apellido,
+        "email"       =>  $request->email,
+        "password"    =>  $request->password,
+        "estado"      =>  "inactivo",
+        "referido"    =>  "",
+        "referente"   =>  Carbon::now()->second.
+          Carbon::now()->minute.
+          Carbon::now()->hour.
+          Carbon::now()->year.
+          Carbon::now()->month.
+          Carbon::now()->day."RY",
+        "validacion"  =>  $CodigoVerificacion,
+        "ciudad"      =>  $request->ciudad
       ])->save();
-
-
-
-
       Session::flash('message', 'Usuario creado correctamente');
-      return Redirect::to('/');       
-     
+      return Redirect::to('/');
     }
-
-   
     Session::flash('error', 'Ocurrio un error inesperado');
     return Redirect::to('/usuarios');
   }
-  public function show($id)
-  {
-
-  }
-
-  public function getCodigoVerificacion()
-  {
-
-    $codigo = Carbon::now()->second.Carbon::now()->minute.Carbon::now()->hour."V";
-    return $codigo;
-  }
-
-  public function InfoEmpresas($user_id)
-  {
-    $info = DB::table('empresas')     
-                ->select('*')    
-                ->where('user_id', '=', $user_id)   
-                ->orderBy('created_at','desc')   
-                ->get();
-
-    return response()->json(
-      $info    
-    );
-  }
-
-  public function dashboard()
-  {
-    $users = DB::table('users')                    
-                ->select('*')    
-                ->where('id', '=', Auth::user()->get()->id)   
-                ->get();
-
-    return view('usuarios.dashboard', compact('users'));    
-  }  
-  public function profile()
-  {
-    return view('usuarios.profile'); 
-  }    
-  public function edit($id)
-  {
-    return view('usuarios.edit', ['user' => $this->user]); 
-  }
-  public function update($id, UserUpdateRequest $request)
-  {
+  public function update($id, UserUpdateRequest $request){
     if(RUT::check($request->rut)){
       $this->user->fill($request->all());
       $this->user->save();
@@ -206,37 +183,27 @@ class UserController extends Controller
       return Redirect::to('/profile');
     }else{
       Session::flash('message-error', 'El rut ingresado no es válido.');
-      return Redirect::to('/profile');   
+      return Redirect::to('/profile');
     }
   }
-  public function ValidarRutUsuario($rut)
-  {
+  public function ValidarRutUsuario($rut){
     if(RUT::check($rut)){
       return RUT::clean($rut);
     }else{
       return "false";
     }
   }
-  public function VerificarUsuario($codigo)
-  {
-
-    if(User::where('validacion', $codigo)->first())
-    {
+  public function VerificarUsuario($codigo){
+    $validaCodigo = User::where('validacion', $codigo)->first();
+    if($validaCodigo){
       DB::table('users')
-              ->where('validacion', $codigo)
-              ->update(['estado' => 'activo']); 
-
+        ->where('validacion', $codigo)
+        ->update(['estado' => 'activo']);
       Session::flash('message', 'Su cuenta ha sido verificada. Disfrute.');
-      return Redirect::to('/login');                 
+      return Redirect::to('/login');
     }
-
-
+    return response()->json(
+      'No se encontró el usuario'
+    );
   }
-  public function destroy($id)
-  {
-    $this->user->delete();
-    Session::flash('message', 'Usuario eliminado correctamente');
-    return Redirect::to('/usuarios');
-  }
-
 }
