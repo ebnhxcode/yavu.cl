a975b2ab84b6f310b2ce59de4ae367960176f4ca
root <root@localhost.localdomain>
2016-04-03 02:40:02 -0300

    Optimizacion Mail Controller

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace yavu\Http\Controllers;
 use Illuminate\Http\Request;
 use Mail;
@@ -9,113 +8,25 @@ use yavu\Http\Requests;
 use yavu\Http\Requests\EnviarMailRequest;
 use yavu\Http\Controllers\Controller;
 use Illuminate\Routing\Route;
-
-
-class MailController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    
-    public function store(EnviarMailRequest $request)
-    {
-       Mail::send('emails.contact', $request->all(), function($msj){
-            $msj->subject('Correo de Contacto');
-            $msj->to('contacto@yavu.cl');
-
-        }); 
-
-       Session::flash('message', 'Mensaje enviado correctamente, gracias :)');
-       return Redirect::to('/contacto');
-    }
-
-
-
-
-
-
-
-
-
-
-
-    public function store(EnviarMailRequest $request)
-    {
-       Mail::send('emails.register', $request->all(), function($msj){
-            $msj->subject('Correo de Registro');
-            $msj->to('contacto@yavu.cl');
-
-        }); 
-
-       Session::flash('message', 'Debe verificar su correo para acceder a yavu.cl! )');
-       return Redirect::to('/contacto');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+class MailController extends Controller{
+  public function index(){
+  }
+  public function create(){
+  }
+  public function destroy($id){
+  }
+  public function edit($id){
+  }
+  public function show($id){
+  }
+  public function store(EnviarMailRequest $request){
+    Mail::send('emails.contact', $request->all(), function($msj){
+      $msj->subject('Correo de Contacto');
+      $msj->to('contacto@yavu.cl');
+    });
+    Session::flash('message', 'Mensaje enviado correctamente, gracias :)');
+    return Redirect::to('/contacto');
+  }
+  public function update(Request $request, $id){
+  }
 }
