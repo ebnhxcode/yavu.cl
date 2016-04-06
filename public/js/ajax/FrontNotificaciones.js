84e48feeca586bd030f7de5f22282ad92f75b2a4
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-04-06 13:22:33 -0300

    Se fué a la vps -> intento 1

--- public/js/ajax/FrontNotificaciones.js
+++ public/js/ajax/FrontNotificaciones.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 		var Contador = 0;
 		var pops = "";
 		$.ajax({
-			url: "http://localhost:8000/cargarpops/"+Global_idUltimaNotificacion+"/"+user_id+"/todas",
+			url: "http://186.64.123.143/cargarpops/"+Global_idUltimaNotificacion+"/"+user_id+"/todas",
 			type: 'GET',
 			dataType: 'json',
 			cache: false,
@@ -119,7 +119,7 @@ $(document).ready(function(){
 	function ContarNotificaciones(){
 		var user_id = $("#user_id").val();
 		$.ajax({
-			url: "http://localhost:8000/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
+			url: "http://186.64.123.143/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
 			type: 'GET',
 			dataType: 'json',
 			cache: false,
