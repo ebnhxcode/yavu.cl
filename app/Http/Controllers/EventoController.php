030ff1243cd61fe0dcae2be8ba0cc9c9aba604a7
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-04-08 14:52:32 -0300

    Optimizacion Evento Controller

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -11,48 +11,58 @@ use Auth;
 use Redirect;
 use Illuminate\Routing\Route;
 use DB;
-class EventoController extends Controller
-{
-    public function __construct(){
-        $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
+class EventoController extends Controller{
+  public function __construct(){
+    $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
+  }
+  public function create(){
+    if(Auth::admin()->check()){
+      return view('eventos.create');
     }
-    public function find(Route $route){
-        $this->evento = Evento::find($route->getParameter('eventos'));
-    }      
-    public function index()
-    {
-        $eventos = Evento::paginate(5);
-        return view('eventos.index', compact('eventos')); 
+    return response()>json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function destroy($id){
+    if(isset($this->evento)){
+      $this->evento->delete();
+      Session::flash('message', 'Evento eliminado correctamente');
+      return Redirect::to('/eventos');
     }
-    public function create()
-    {
-        return view('eventos.create');
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function edit($id){
+    if(isset($this->evento) && Auth::admin()->check()){
+      return view('eventos.edit', ['evento' => $this->evento]);
     }
-    public function store(EventoCreateRequest $request)
-    {
-        Evento::create($request->all());
-        Session::flash('message', 'Evento creado correctamente');
-        return Redirect::to('/eventos'); 
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function find(Route $route){
+    $this->evento = Evento::find($route->getParameter('eventos'));
+  }
+  public function index(){
+    if(Auth::admin()->check()){
+      $eventos = Evento::paginate(5);
+      return view('eventos.index', compact('eventos'));
     }
-    public function show($id)
-    {
-        
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function show($id){
+    return Redirect::to('/');
+  }
+  public function store(EventoCreateRequest $request){
+    if(isset($request)){
+      Evento::create($request->all());
+      Session::flash('message', 'Evento creado correctamente');
+      return Redirect::to('/eventos');
     }
-    public function edit($id)
-    {
-        return view('eventos.edit', ['evento' => $this->evento]);
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function update(EventoUpdateRequest $request, $id){
+    if(isset($request) && Auth::admin()->check()){
+      $this->evento->fill($request->all());
+      $this->evento->save();
+      Session::flash('message', 'Evento editado correctamente');
+      return Redirect::to('/eventos');
     }
-    public function update(EventoUpdateRequest $request, $id)
-    {
-        $this->evento->fill($request->all());
-        $this->evento->save();
-        Session::flash('message', 'Evento editado correctamente');
-        return Redirect::to('/eventos');
-    }
-    public function destroy($id)
-    {
-        $this->evento->delete();
-        Session::flash('message', 'Evento eliminado correctamente');
-        return Redirect::to('/eventos');
-    }
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
 }
