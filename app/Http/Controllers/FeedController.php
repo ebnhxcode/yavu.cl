9583fc5c1697c67469cf1a591af278ca6ebd32ed
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-04-08 14:43:19 -0300

    Optimizacion Feed Controller

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -16,87 +16,84 @@ class FeedController extends Controller{
   public function __construct(){
     $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
   }
+  public function CargarFeeds($idUltima){
+    if(isset($idUltima) && Auth::user()->check()){
+      $idUltima = addslashes($idUltima);
+
+      if((int) $idUltima == "0"){
+        $feeds = DB::table('estado_empresas')
+          ->join('empresas'  , 'empresas.id', '=', 'estado_empresas.empresa_id')
+          ->select('estado_empresas.*', 'empresas.nombre as nombreEmp', 'empresas.imagen_perfil as imagen_perfil_empresa')
+          ->where('estado_empresas.id', '>', (int) $idUltima)
+          ->orderBy('estado_empresas.created_at','desc')
+          ->limit('10')
+          ->get();
+
+      }elseif((int) $idUltima <> "0"){
+        $feeds = DB::table('estado_empresas')
+          ->join('empresas'  , 'empresas.id', '=', 'estado_empresas.empresa_id')
+          ->select('estado_empresas.*', 'empresas.nombre as nombreEmp', 'empresas.imagen_perfil as imagen_perfil_empresa')
+          ->where('estado_empresas.id', '<', (int) $idUltima)
+          ->orderBy('estado_empresas.created_at','desc')
+          ->limit('10')
+          ->get();
+      }
+      return response()->json($feeds);
+    }
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function CargarFeedsEmpresa($idUltima, $empresa){
+    return Redirect::to('/');
+    return 'Cargar feeds empresa';
+  }
+  public function create(){
+    return Redirect::to('/');
+  }
+  public function destroy($id){
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+    if(isset($this->feed)){
+      $this->feed->delete();
+      Session::flash('message', 'Feed eliminado correctamente');
+      return Redirect::to('/feeds');
+    }
+  }
+  public function edit($id){
+    return Redirect::to('/');
+    //return view('feeds.edit', ['feed' => $this->feed]);
+  }
+  public function EliminarFeed($id){
+    if(isset($id) && $id !== ""){
+      $id = addslashes($id);
+      DB::table('estado_empresas')->where('id', '=', $id)->delete();
+      return response()->json(["Mensaje: " => "Eliminado"]);
+    }
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
   public function find(Route $route){
     $this->feed = Feed::find($route->getParameter('feeds'));
     //return $this->user;
   }
   public function index(){
-    return view('feeds.index');
-  }
-
-  //select * from estado_empresas where empresa_id in (select empresa_id from followers where user_id = 18)
-
-  public function CargarFeeds($idUltima){
-    if(isset($idUltima)){
-      $idUltima = addslashes($idUltima);
+    if(Auth::user()->check()){
+      return view('feeds.index');
     }
-
-    if((int) $idUltima == "0"){
-      $feeds = DB::table('estado_empresas')
-        ->join('empresas'  , 'empresas.id', '=', 'estado_empresas.empresa_id')
-        ->select('estado_empresas.*', 'empresas.nombre as nombreEmp', 'empresas.imagen_perfil as imagen_perfil_empresa')
-        ->where('estado_empresas.id', '>', (int) $idUltima)
-        ->orderBy('estado_empresas.created_at','desc')
-        ->limit('10')
-        ->get();
-    }elseif((int) $idUltima <> "0"){
-      $feeds = DB::table('estado_empresas')
-        ->join('empresas'  , 'empresas.id', '=', 'estado_empresas.empresa_id')
-        ->select('estado_empresas.*', 'empresas.nombre as nombreEmp', 'empresas.imagen_perfil as imagen_perfil_empresa')
-        ->where('estado_empresas.id', '<', (int) $idUltima)
-        ->orderBy('estado_empresas.created_at','desc')
-        ->limit('10')
-        ->get();
-    }
-    return response()->json(
-      $feeds
-    );
-  }
-
-  public function CargarFeedsEmpresa($idUltima, $empresa){
-    return 'Cargar feeds empresa';
-  }
-
-  public function create(){
-    return view('feeds.create');
-  }
-
-  public function store(FeedCreateRequest $request){
-    if($request->ajax()){
-      EstadoEmpresa::create($request->all());
-      //Session::flash('message', 'Publicacion creada correctamente');
-      return response()->json([
-        "Mensaje: " => "Creado"
-      ]);
-      //CargarEstadoEmpresa();
-    }
+    return Redirect::to('/');
   }
   public function show($id){
+    return Redirect::to('/');
   }
-  public function EliminarFeed($id){
-    $id = addslashes($id);
-    if(isset($id) && $id !== ""){
-      DB::table('estado_empresas')->where('id', '=', $id)->delete();
-      return response()->json([
-        "Mensaje: " => "Eliminado"
-      ]);
+  public function store(FeedCreateRequest $request){
+    if(isset($request) && $request->ajax()){
+      EstadoEmpresa::create($request->all());
+      return response()->json(["Mensaje: " => "Creado"]);
     }
-    return response()->json([
-      "Mensaje: " => "Error"
-    ]);
-  }
-  public function edit($id){
-    return view('feeds.edit', ['feed' => $this->feed]);
+    return response()->json(["Mensaje: " => "Acceso denegado"]);
   }
   public function update(FeedUpdateRequest $request, $id){
+    return Redirect::to('/');
     $this->feed->fill($request->all());
     $this->feed->save();
     Session::flash('message', 'Feed editado correctamente');
-    return Redirect::to('/feeds');
-  }
-  public function destroy($id){
-    $this->feed->delete();
-    Session::flash('message', 'Feed eliminado correctamente');
     return Redirect::to('/feeds');
   }
 }
