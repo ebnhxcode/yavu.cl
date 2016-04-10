f3342615af617bab71e6accf1b8abfa4ed7e8ebd
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-04-10 17:08:32 -0300

    Optimizacion Beneficio Controller

--- app/Http/Controllers/BeneficioController.php
+++ app/Http/Controllers/BeneficioController.php
@@ -11,48 +11,68 @@ use Auth;
 use Redirect;
 use Illuminate\Routing\Route;
 use DB;
-class BeneficioController extends Controller
-{
-    public function __construct(){
-        $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
+class BeneficioController extends Controller{
+  public function __construct(){
+    $this->beforeFilter('@find', ['only' => ['edit', 'update', 'destroy']]);
+  }
+  public function find(Route $route){
+    if(Auth::admin()->check()){
+      $this->beneficio = Beneficio::find($route->getParameter('beneficios'));
     }
-    public function find(Route $route){
-        $this->beneficio = Beneficio::find($route->getParameter('beneficios'));
-    }      
-    public function index()
-    {
-        $beneficios = Beneficio::paginate(5);
-        return view('beneficios.index', compact('beneficios'));        
+    return Redirect::to("/");
+    //return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function index(){
+    if(Auth::admin()->check()){
+      $beneficios = Beneficio::paginate(5);
+      return view('beneficios.index', compact('beneficios'));
     }
-    public function create()
-    {
-        return view('beneficios.create');
+    return Redirect::to("/");
+    //return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function create(){
+    if(Auth::admin()->check()){
+      return view('beneficios.create');
     }
-    public function store(BeneficioCreateRequest $request)
-    {
-        Beneficio::create($request->all());
-        Session::flash('message', 'Beneficio creado correctamente');
-        return Redirect::to('/beneficios');
+    return Redirect::to("/");
+    //return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function store(BeneficioCreateRequest $request){
+    if(Auth::admin()->check()){
+      Beneficio::create($request->all());
+      Session::flash('message', 'Beneficio creado correctamente');
+      return Redirect::to('/beneficios');
     }
-    public function show($id)
-    {
-        
+    return Redirect::to("/");
+    //return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function show($id){
+
+  }
+  public function edit($id){
+    if(Auth::admin()->check()){
+      return view('beneficios.edit', ['beneficio' => $this->beneficio]);
     }
-    public function edit($id)
-    {
-        return view('beneficios.edit', ['beneficio' => $this->beneficio]);
+    return Redirect::to("/");
+    //return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function update(BeneficioUpdateRequest $request, $id){
+    if(Auth::admin()->check()){
+      $this->beneficio->fill($request->all());
+      $this->beneficio->save();
+      Session::flash('message', 'Beneficio editado correctamente');
+      return Redirect::to('/beneficios');
     }
-    public function update(BeneficioUpdateRequest $request, $id)
-    {
-        $this->beneficio->fill($request->all());
-        $this->beneficio->save();
-        Session::flash('message', 'Beneficio editado correctamente');
-        return Redirect::to('/beneficios');
+    return Redirect::to("/");
+    //return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
+  public function destroy($id){
+    if(Auth::admin()->check()){
+      $this->beneficio->delete();
+      Session::flash('message', 'Beneficio eliminado correctamente');
+      return Redirect::to('/beneficios');
     }
-    public function destroy($id)
-    {
-        $this->beneficio->delete();
-        Session::flash('message', 'Beneficio eliminado correctamente');
-        return Redirect::to('/beneficios');
-    }
+    return Redirect::to("/");
+    //return response()->json(["Mensaje: " => "Acceso denegado"]);
+  }
 }
