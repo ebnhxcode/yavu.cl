fdc02e6844322215a2030d9a9274d0a4273e7395
Panic <panic@MacBook-Pro.home>
2016-04-25 14:56:20 -0300

    modificaciones al correo de registo

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -5,5 +5,5 @@
 	<p><strong>Para validar tu cuenta haz click en el enlace de validación: </strong><a href="http://localhost:8000/verificarusuario/{!! $codigo !!}"> Abrir </a>  </p>
 	</body>
 	<strong>¡Muchas gracias por tu registo y disfruta de los beneficios que tenemos para ofrecerte!</strong>
-	<h1>Equipo Yavu.cl</h1>
+	<h2>Equipo Yavu.cl</h2>
 </html>
