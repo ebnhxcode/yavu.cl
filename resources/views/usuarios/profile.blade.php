a2cd9aab9e5a0972685264d973c8032ae58f66ce
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-05-02 16:27:47 -0300

    vps

--- resources/views/usuarios/profile.blade.php
+++ resources/views/usuarios/profile.blade.php
@@ -235,7 +235,7 @@
 				var status_id = valor.replace('estado_','');
 				var user_id = $('#user_id').val();
 				var token = $('#token').val();
-				var route = 'http://localhost:8000/interactuar';
+				var route = 'http://yavu.cl/interactuar';
 				$.ajax({
 					url: route,
 					headers: {'X-CSRF-TOKEN': token},
@@ -258,7 +258,7 @@
 
 			function ContarInteracciones(status_id){
 				status_id = status_id;
-				var route = 'http://localhost:8000/contarinteracciones/'+status_id;
+				var route = 'http://yavu.cl/contarinteracciones/'+status_id;
 				var user_id = $('#user_id');
 				var Contador = 0;
 				$.get(route, function(res){
