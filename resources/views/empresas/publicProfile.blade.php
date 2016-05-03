ada7b71064605cb1a77c380f1d1ce322c59ac750
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-05-03 15:53:09 -0300

    vps test

--- resources/views/empresas/publicProfile.blade.php
+++ resources/views/empresas/publicProfile.blade.php
@@ -281,7 +281,7 @@
   /*FUNCIONES Y PROCEDIMIENTOS*/
   function ContarInteracciones(status_id){
     status_id = status_id;
-    var route = "http://localhost:8000/contarinteracciones/"+status_id;
+    var route = "http://yavu.cl/contarinteracciones/"+status_id;
     var user_id = $("#user_id");
     var Contador = 0;
     $.get(route, function(res){
@@ -295,7 +295,7 @@
   function ContarNotificaciones(){
     var user_id = $("#user_id").val();
     $.ajax({
-      url: "http://localhost:8000/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
+      url: "http://yavu.cl/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
       type: 'GET',
       dataType: 'json',
       cache: false,
@@ -316,7 +316,7 @@
     return true;
   }
   function ContarCoins(){
-    var route = "http://localhost:8000/contarcoins";
+    var route = "http://yavu.cl/contarcoins";
     var user_id = $("#user_id");
     $.get(route, function(res){
       $(".CantidadCoins").text("");
@@ -335,7 +335,7 @@
 
 
     if( user_anon == user_id){
-      var route = "http://localhost:8000/eliminarfeed/"+id;
+      var route = "http://yavu.cl/eliminarfeed/"+id;
       $.ajax({
         url: route,
         type: 'GET',
@@ -355,7 +355,7 @@
     var status_id = valor.replace('estado_','');
     var user_id = $("#user_id").val();
     var token = $("#token").val();
-    var route = "http://localhost:8000/interactuar";
+    var route = "http://yavu.cl/interactuar";
     $.ajax({
       url: route,
       headers: {'X-CSRF-TOKEN': token},
