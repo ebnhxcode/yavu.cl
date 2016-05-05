8e7054d23fd3eaf22f53aedb96ffc8e2c720767e
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-05-04 23:52:21 -0300

    respaldo

--- resources/views/miniDashboard/miniDashboard.blade.php
+++ resources/views/miniDashboard/miniDashboard.blade.php
@@ -1,17 +1,65 @@
 <div class="list-group-item">
   <div class="row">
     <!--
+     empresas
      sorteos
-     crear sorteo
      tickets
      feeds
-     empresas
+
+     crear sorteo
+
+
      crear empresa
      informes
      configuraciones
 
      -->
 
+    @if(Request::path() != 'login')
+    @endif
+
+    <div class="col-md-4 col-sm-2 col-xs-3">
+      <div class='list-group' >
+        <div align="center">
+          <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/usuarios/'.Auth::user()->get()->id.'/edit')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
+            <span>
+              <img width="80%" src= "{!!URL::to('img/dash/ico_configurar_dash02.png')!!}"/>
+            </span>
+          </a>
+        </div>
+        <div align="center"><small>Configuraciones</small></div>
+      </div>
+    </div>
+
+    @if(Request::path() != 'feeds')
+      <div class="col-md-4 col-sm-2 col-xs-3">
+        <div class="list-group" >
+          <div align="center">
+            <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/feeds')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
+              <span>
+                <img width="80%" src= "{!!URL::to('img/dash/ico_pin03.png')!!}"/>
+              </span>
+            </a>
+          </div>
+          <div align="center"><small>Publicaciones</small></div>
+        </div>
+      </div>
+    @endif
+    @if(Request::path() != 'empresas')
+    <div class="col-md-4 col-sm-2 col-xs-3">
+      <div class="list-group" >
+        <div align="center">
+          <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/empresas')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
+            <span>
+              <img width="80%" src= "{!!URL::to('img/dash/ico_empresa.png')!!}"/>
+            </span>
+          </a>
+        </div>
+        <div align="center"><small>Empresas</small></div>
+      </div>
+    </div>
+    @endif
+    @if(Request::path() != 'sorteos*')
     <div class="col-md-4 col-sm-2 col-xs-3">
       <div class="list-group" >
         <div align="center">
@@ -24,6 +72,50 @@
         <div align="center"><small>Sorteos</small></div>
       </div>
     </div>
+    @endif
+    @if(Request::path() != 'tickets*')
+    <div class="col-md-4 col-sm-2 col-xs-3">
+      <div class="list-group" >
+        <div align="center">
+          <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/tickets/history')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
+            <span>
+              <img width="80%" src= "{!!URL::to('img/dash/ico_ticket01.png')!!}"/>
+            </span>
+          </a>
+        </div>
+        <div align="center"><small>Tickets</small></div>
+      </div>
+    </div>
+    @endif
+    @if(Request::path() != 'coins*')
+    <div class="col-md-4 col-sm-2 col-xs-3">
+      <div class='list-group' >
+        <div align="center">
+          <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/coins/history')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
+            <span>
+              <img width="80%" src= "{!!URL::to('img/dash/ico_notificacion004.png')!!}"/>
+            </span>
+          </a>
+        </div>
+        <div align="center"><small>Informes</small></div>
+      </div>
+    </div>
+    @endif
+    @if(Request::path() != 'coins*')
+    <div class="col-md-4 col-sm-2 col-xs-3">
+      <div class='list-group' >
+        <div align="center">
+          <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/tickets/history')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
+            <span>
+              <img width="80%" src= "{!!URL::to('img/dash/icono_informe01.png')!!}"/>
+            </span>
+          </a>
+        </div>
+        <div align="center"><small>Informes</small></div>
+      </div>
+    </div>
+    @endif
+
 
     <div class="col-md-4 col-sm-2 col-xs-3">
       <div class="list-group" >
@@ -38,97 +130,12 @@
       </div>
     </div>
 
-    <div class="col-md-4 col-sm-2 col-xs-3">
-      <div class="list-group" >
-        <div align="center">
-          <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/tickets/history')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
-            <span>
-              <img width="80%" src= "{!!URL::to('img/dash/ico_ticket01.png')!!}"/>
-            </span>
-          </a>
-        </div>
-        <div align="center"><small>Tickets</small></div>
-      </div>
-    </div>
-
-    <div class="col-md-4 col-sm-2 col-xs-3">
-      <div class="list-group" >
-        <div align="center">
-          <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/empresas')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
-            <span>
-              <img width="80%" src= "{!!URL::to('img/dash/ico_empresa.png')!!}"/>
-            </span>
-          </a>
-        </div>
-        <div align="center"><small>Empresas</small></div>
-      </div>
-    </div>
 
 
-    <div class="col-md-4 col-sm-2 col-xs-3">
-      <div class="list-group" >
-        <div align="center">
-          <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/feeds')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
-            <span>
-              <img width="80%" src= "{!!URL::to('img/dash/ico_pin03.png')!!}"/>
-            </span>
-          </a>
-        </div>
-        <div align="center"><small>Publicaciones</small></div>
-      </div>
-    </div>
 
-    <div class="col-md-4 col-sm-2 col-xs-3">
-      <div class='list-group' >
-        <div align="center">
-          <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/coins/history')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
-            <span>
-              <img width="80%" src= "{!!URL::to('img/dash/ico_notificacion004.png')!!}"/>
-            </span>
-          </a>
-        </div>
-        <div align="center"><small>Informe Coins</small></div>
-      </div>
-    </div>
 
-    <div class="col-md-4 col-sm-2 col-xs-3">
-      <div class='list-group' >
-        <div align="center">
-          <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/tickets/history')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
-            <span>
-              <img width="80%" src= "{!!URL::to('img/dash/icono_informe01.png')!!}"/>
-            </span>
-          </a>
-        </div>
-        <div align="center"><small>Informe Ticket's</small></div>
-      </div>
-    </div>
 
-    <div class="col-md-4 col-sm-2 col-xs-3">
-      <div class='list-group' >
-        <div align="center">
-          <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/tickets/history')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
-            <span>
-              <img width="80%" src= "{!!URL::to('img/dash/icono_informe01.png')!!}"/>
-            </span>
-          </a>
-        </div>
-        <div align="center"><small>Informe Ticket's</small></div>
-      </div>
-    </div>
 
-    <div class="col-md-4 col-sm-2 col-xs-3">
-      <div class='list-group' >
-        <div align="center">
-          <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/usuarios/'.Auth::user()->get()->id.'/edit')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
-            <span>
-              <img width="80%" src= "{!!URL::to('img/dash/ico_configurar_dash02.png')!!}"/>
-            </span>
-          </a>
-        </div>
-        <div align="center"><small>Modificar perfil</small></div>
-      </div>
-    </div>
 
 
 
