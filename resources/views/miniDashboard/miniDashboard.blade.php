fb3c66b1bdeed6aa34d883b0cb6155618085e918
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-05-04 21:35:32 -0300

    Respaldo antes de cambiar el minidashboard

--- resources/views/miniDashboard/miniDashboard.blade.php
+++ resources/views/miniDashboard/miniDashboard.blade.php
@@ -1,5 +1,29 @@
 <div class="list-group-item">
   <div class="row">
+    <!--
+     sorteos
+     crear sorteo
+     tickets
+     feeds
+     empresas
+     crear empresa
+     informes
+     configuraciones
+
+     -->
+
+    <div class="col-md-4 col-sm-4 col-xs-4">
+      <div class="list-group" >
+        <div align="center">
+          <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/sorteos')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
+            <span>
+              <img width="80%" src= "{!!URL::to('img/dash/ico_sorteo01.png')!!}"/>
+            </span>
+          </a>
+        </div>
+        <div align="center"><small>Sorteos</small></div>
+      </div>
+    </div>
 
     <div class="col-md-4 col-sm-4 col-xs-4">
       <div class="list-group" >
@@ -26,6 +50,20 @@
         <div align="center"><small>Tickets</small></div>
       </div>
     </div>
+
+    <div class="col-md-4 col-sm-4 col-xs-4">
+      <div class="list-group" >
+        <div align="center">
+          <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/empresas')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
+            <span>
+              <img width="80%" src= "{!!URL::to('img/dash/ico_empresa.png')!!}"/>
+            </span>
+          </a>
+        </div>
+        <div align="center"><small>Empresas</small></div>
+      </div>
+    </div>
+
 
     <div class="col-md-4 col-sm-4 col-xs-4">
       <div class="list-group" >
