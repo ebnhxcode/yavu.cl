c69036bd95ecb6b767e51b601d860a9084bebe84
Panic <panic@MacBook-Pro.local>
2016-05-05 23:06:24 -0300

    Cambios en graficos basicos

--- resources/views/empresas/companyStatistics.blade.php
+++ resources/views/empresas/companyStatistics.blade.php
@@ -16,69 +16,99 @@
           </div> <!-- /panel -->
         </div>
 
-
         <div class="col-md-8 col-sm-12 col-xs-12">
 
           <div class="list-group">
             <div class="list-group-item list-group-item-success">
               ESTADISTICAS B&Aacute;SICAS
             </div>
+              <br>
             <div class="list-group-item">
+
+            VISITAS <i class="glyphicon glyphicon-eye-open"></i>
+            <hr>
               <div class="row">
+               
+                <div class="col-sm-6">
+                  <div class="hero-widget well well-sm">
+                    <div>
+                     <i class="glyphicon glyphicon-eye-open"></i>
+                    </div>
 
-
-
-                  <div class="col-md-4">
-                    Total de visitas : {!! $statistics[3] !!}<br>
+                  <div>
+                    <label class="text-muted">Total de visitas:<br></label>
+                    <var>{!! $statistics[3] !!}</var>
                   </div>
-
-                  <div class="col-md-8">
-                    Visitas por parte de Hombres : {!! $statistics[0] !!}<br>
-                    Visitas por parte de Mujeres : {!! $statistics[1] !!}<br>
-                    Visitas sin sexo definido : {!! $statistics[2] !!}<br>
-                  </div>
-
-
-                  <div class="col-md-4">
-                    Alcance publicitario : {!! $statistics[4] !!}<br>
-                  </div>
-
-                  <div class="col-md-8">
-                    a
-                  </div>
-
-
-
-                  <div class="col-md-4">
-                    Coins Otorgadas : {!! (int) $statistics[4] * 10 !!}<br>
-                  </div>
-
-                  <div class="col-md-8">
-                    a
-
-                  </div>
-
-
-
+                </div>
                 </div>
 
-
+                <div class="col-sm-6">
+                  <div class="hero-widget well well-sm">
+                    <div>
+                      <i class="glyphicon glyphicon-user"></i>
+                    </div>
+                      <div>
+                        <label class="text-muted">Total de visitas por sexo:<br></label>
+                            <div>
+                              <var>Visitas de sexo Masculino : {!! $statistics[0] !!}</var><br>
+                              <var>Visitas de sexo Femenino: {!! $statistics[1] !!}</var><br>
+                              <var>Visitas sin sexo definido : {!! $statistics[2] !!}</var>                              
+                            </div>
+                      </div>
+                  </div>
+                </div>
               </div>
-
-
-
-
-
             </div>
 
-          </div>
+            <div class="list-group-item">
+            ALCANCE <i class="glyphicon glyphicon-plane"></i>
+            <hr>
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="hero-widget well well-sm">
+                    <div>
+                     <i class="glyphicon glyphicon-plane"></i>
+                    </div>
+                  <div>
+                    <label class="text-muted">Total de alcance publicitario:<br></label>
+                    <var>{!! $statistics[4] !!}</var>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
 
+            <div class="list-group-item">
+            BENEFICIOS OTORGADOS <i class="glyphicon glyphicon-plane" ></i>
+            <hr>
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="hero-widget well well-sm">
+                    <div>
+                     <i class="glyphicon glyphicon-plane"></i>
+                    </div>
+                  <div>
+                    <label class="text-muted">Total Coins Otorgadas:<br></label>
+                    <var>{!! (int) $statistics[4] * 10 !!}</var>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
         </div>
       </div>
     </div>
   </div>
 
 
+
+
+
+
+                  
 
 
 
