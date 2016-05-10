cebe045e1befaa69fced97d2738532f63400857e
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-05-10 12:35:03 -0300

    Test15 socialite

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -41,7 +41,7 @@ class SocialController extends Controller{
   //public function getSocialAuthCallback($provider=null){
   public function getSocialAuthCallback($provider){
 
-    dd(Socialite::driver('facebook')->user());
+    //dd(Socialite::driver('facebook')->user());
 
     $user = Socialite::driver('facebook')->user();
 
