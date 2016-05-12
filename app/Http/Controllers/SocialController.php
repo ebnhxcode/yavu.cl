3157880ffaeffbec708ee9aabc5bdac545b07c12
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-05-12 14:49:55 -0300

    empty email

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -47,7 +47,7 @@ class SocialController extends Controller{
 
     $user = Socialite::driver($provider)->user();
 
-    if(!empty($user) && $user->email){
+    if(!empty($user) && !empty($user->email)){
 
 
 
