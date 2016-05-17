bd7e1b03f5128fc577b41cf3803d6a79cd2914a9
Esteban Ramos Fernández <esteban.ramos@milahorros.cl>
2016-05-17 16:43:07 -0400

    FIX user edit 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -89,7 +89,7 @@ class UserController extends Controller{
    * @private
    */
   private function getNormalSessionData(){
-    return User::where('estado', 'Activo')->where('id', Auth::user()->get()->id)->select('id','nombre','email','ciudad','imagen_perfil','imagen_portada')->get();
+    return User::where('estado', 'Activo')->where('id', Auth::user()->get()->id)->select('id','nombre','email','ciudad','imagen_perfil','imagen_portada')->get()->first();
   }
 
   /**
