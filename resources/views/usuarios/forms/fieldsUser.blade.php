a38ce495de493be974f2574fdc33303cfc6c2eda
Esteban Ramos Fernández <esteban.ramos@milahorros.cl>
2016-05-17 15:03:30 -0400

    ADD ultimos cambios para vps

--- resources/views/usuarios/forms/fieldsUser.blade.php
+++ resources/views/usuarios/forms/fieldsUser.blade.php
@@ -234,7 +234,7 @@
 	});
 	function ValidarRut(rut){
 		console.log('click');
-		var route = 'http://localhost:8000/validarrutusuario/'+rut+'';
+		var route = 'http://yavu.cl/validarrutusuario/'+rut+'';
 		document.getElementById('FormUsuario').onsubmit = function() {
 		    return false;
 		}	
