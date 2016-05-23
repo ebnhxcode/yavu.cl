bdf95258aa6a8ffbce99b683c5e006cf8da97684
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-05-23 13:48:15 -0400

    Optimizando modulo de administracion

--- resources/views/admins/index.blade.php
+++ resources/views/admins/index.blade.php
@@ -1,37 +1,82 @@
- @extends('layouts.frontadm')	
+@extends('layouts.frontadm')
 @section('content')
 <div class="jumbotron">
-	<div id="contentIn">
-		@include('alerts.alertFields')
-		@include('alerts.errorsMessage')
-		@include('alerts.successMessage')
-		@include('alerts.warningMessage')	
-			<div class="col-lg-2 col-sm-2"> <h6>Administración</h6> <a href="#"> 
-			<img src= "{!!URL::to('img/yavu007.png')!!}" height="100px" width="100px"/></a>
-			</div>	
-
-
-			<div class="panel-body">
-				<table class="table">
-					<thead>
-					<h5>El panel de administración es una interfaz intuitiva para configurar y administrar las empresas, servicios y estados de pagos de estas mismas. Esto es su base para todo lo relacionado con la plataforma y poder administrar de manera correcta Yavu.cl.</h5>
-						<th>Nombre</th>
-						<th>Correo</th>
-						<th>Operaciones</th>
-					</thead>
-					@foreach($admins as $admin)	
-					<tbody>
-						<td>{!!$admin->nombre!!}</td>
-						<td>{!!$admin->email!!}</td>
-						<td>{!!link_to_route('admins.edit', $title = 'Editar', $parameters = $admin->id, $attributes = ['class'=>'btn btn-primary btn-lg'])!!}</td>
-					</tbody>
-					@endforeach
-				</table>	
-				{!!$admins->render()!!}	
+	<div id="contentMiddle">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				@include('alerts.alertFields')
+				@include('alerts.errorsMessage')
+				@include('alerts.successMessage')
+				@include('alerts.warningMessage')
+				@include('alerts.infoMessage')
+				<div class="" style="font-size: 3em;">
+					<img id="img" style="padding-bottom: 20px;" width="8%" src= "{!!URL::to('img/newGraphics/neo_icono_config02.png')!!}"/><span> <a href="{!! URL::to('/admins') !!}">Administraci&oacute;n Yavü</a></span>
+				</div>
 			</div>
-	
-	</div>
-</div>
+
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="list-group">
+					<div class="list-group-item">
+						<h3>Lista de administradores</h3>
+					</div>
+					<div class="list-group-item">
+						<div class="wrap">
+							<table class="table">
+								<thead>
+								<th>ID</th>
+								<th>Nombre</th>
+								<th>Correo</th>
+								<th>Operaciones</th>
+								</thead>
+								@foreach($admins as $admin)
+									<tbody>
+									<td>{!!$admin->id!!}</td>
+									<td>{!!$admin->nombre.' '.$admin->apellido!!}</td>
+									<td>{!!$admin->email!!}</td>
+									<td>{!!link_to_route('admins.edit', $title = 'Editar', $parameters = $admin->id, $attributes = ['class'=>'btn btn-primary btn-sm'])!!}</td>
+									</tbody>
+								@endforeach
+							</table>
+							{!!$admins->render()!!}
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="list-group">
+					<div class="list-group-item">
+						<h3>Informaci&oacute;n b&aacute;sica de entrada</h3>
+					</div>
+					<div class="list-group-item">
+
+					</div>
+				</div>
+
+				<div class="list-group">
+					<div class="list-group-item">
+						<h3>Accesos rápidos</h3>
+					</div>
+					<div class="list-group-item">
+						<a href="{!! URL::to('/admins/create') !!}">Crear administrador</a>
+					</div>
+					<div class="list-group-item">
+						<a href="{!! URL::to('/sorteospendientes') !!}">Confirmar sorteos</a>
+					</div>
+					<div class="list-group-item">
+						<a href="{!! URL::to('#!') !!}">Listar empresas</a>
+					</div>
+					<div class="list-group-item">
+						<a href="{!! URL::to('#!') !!}">Pre-Crear empresa</a>
+					</div>
+					<div class="list-group-item">
+						<a href="{!! URL::to('#!') !!}"></a>
+					</div>
+				</div><!-- /div list-group -->
+			</div><!-- /div col-md-6-sm-6-xs-12 -->
+		</div><!-- /div row -->
+	</div><!-- /div contentMiddle -->
+</div><!-- /div jumbotron -->
 
 
 @stop
--- resources/views/tickets/history.blade.php
+++ resources/views/tickets/history.blade.php
@@ -6,9 +6,11 @@
     @include('alerts.errorsMessage')
     @include('alerts.successMessage')
     @include('alerts.warningMessage')
+    @include('alerts.infoMessage')
     <div class="" style="font-size: 3em;">
       <img id="img" style="padding-bottom: 20px;" width="8%" src= "{!!URL::to('img/newGraphics/neo_icono_tickets.png')!!}"/><span> <a href="{!! URL::to('tickets') !!}">Ticket's</a><span class="requerido">\</span> Historial</span>
     </div>
+
     <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12">
       <!--
