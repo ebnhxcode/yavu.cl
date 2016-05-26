2d022c806ef38ca5087e944c29208dd1aa5efa03
Panic <panic@MacBook-Pro.home>
2016-05-26 11:26:01 -0400

    Datos de los modelos de link y category

--- app/CategoryBannerData.php
+++ app/CategoryBannerData.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace yavu;
-
 use Illuminate\Database\Eloquent\Model;
 
 class CategoryBannerData extends Model
 {
-    //
+    protected $table = 'category_banner_data';
+    protected $primaryKey = 'id';
+    protected $fillable = array( 'banner_data_id','category');	
 }
