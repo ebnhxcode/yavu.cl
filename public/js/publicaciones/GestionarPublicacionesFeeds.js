d0b7af29137baae87c5df0436ef99b0025209d31
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-06-01 14:01:18 -0400

    Local

--- public/js/publicaciones/GestionarPublicacionesFeeds.js
+++ public/js/publicaciones/GestionarPublicacionesFeeds.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	var Global_ContadorCargaPublicaciones;
 	var Global_Control = true;
 	var Refresh = 100;
+
 /*DECLARACION DE VARIABLES GLOBALES*/
 
 /*MÉTODOS CONSTRUCTORES*/
@@ -44,7 +45,7 @@ $(document).ready(function(){
 			var user_id = $("#user_id").val();
 			var empresa_id = $("#empresa_id").val();
 			var token = $("#token").val();
-			var route = "http://yavu.cl/estadoempresa";
+			var route = "http://localhost:8000/estadoempresa";
 			$.ajax({
 				url: route,
 				headers: {'X-CSRF-TOKEN': token},
@@ -97,10 +98,12 @@ $(document).ready(function(){
 /*SELECTORES*/
 
 /*FUNCIONES Y PROCEDIMIENTOS*/
+
+	/*
 	function ActualizarEstados(){
 		var EstadosUsuario = $("#Estados").val(); 
 		$("#Estados").value = "";
-		var route = "http://yavu.cl/estadosusuario";
+		var route = "http://localhost:8000/estadosusuario";
 		var user_id = $("#user_id");
 		var Contador = 0;
 		$.get(route, function(res){
@@ -114,7 +117,7 @@ $(document).ready(function(){
 					"<div id='status' class='list-group'>"
 						+"<div class='list-group-item'>"	
 							+"<h4><a href='/profile' style='color:#3C5B28;'>"
-								+"<img class='media-object' src='http://yavu.cl/images/user.png' data-holder-rendered='true' style='width: 32px; height: 32px;'/>"
+								+"<img class='media-object' src='http://localhost:8000/images/user.png' data-holder-rendered='true' style='width: 32px; height: 32px;'/>"
 								+value.nombre+" "+value.apellido
 							+"</a></h4>"
 							+"<small>"
@@ -133,10 +136,11 @@ $(document).ready(function(){
 			});
 		});
 	}
+	*/
 
 	function CargarEstados(){
 		var Estados = $("#Estados");
-		var route = "http://yavu.cl/cargarfeeds/"+$("#idUltima").val();
+		var route = "http://localhost:8000/cargarfeeds/"+$("#idUltima").val();
 		var Contador = 0;
 		var user_id = $("#user_id").val();
 		$.get(route, function(res){
@@ -218,7 +222,7 @@ $(document).ready(function(){
 /*
   function ContarInteracciones(status_id){
     status_id = status_id;
-    var route = "http://yavu.cl/contarinteracciones/"+status_id;
+    var route = "http://localhost:8000/contarinteracciones/"+status_id;
     var user_id = $("#user_id").val();
     var Contador = 0;
     $.get(route, function(res){
@@ -239,7 +243,7 @@ $(document).ready(function(){
 	function ContarEstados(){
 		var CargarEstados = $("#CargarEstados");
 		var EstadosNuevos = ("#EstadosNuevos");
-		var route = "http://yavu.cl/contarestados";
+		var route = "http://localhost:8000/contarestados";
 		var user_id = $("#user_id");
 		var Contador = 0;
 		$.get(route, function(res){
