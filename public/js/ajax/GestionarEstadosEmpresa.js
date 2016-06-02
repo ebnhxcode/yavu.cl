676e6f3f246581ec0089dd1868fc2c1e649eaedd
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-06-02 15:24:28 -0400

    Cambios a Local

--- public/js/ajax/GestionarEstadosEmpresa.js
+++ public/js/ajax/GestionarEstadosEmpresa.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
 			var user_id = $("#user_id").val();
 			var empresa_id = $("#empresa_id").val();
 			var token = $("#token").val();
-			var route = "http://186.64.123.143/estadoempresa";
+			var route = "http://localhost:8000/estadoempresa";
 			$.ajax({
 				url: route,
 				headers: {'X-CSRF-TOKEN': token},
@@ -97,7 +97,7 @@ $(document).ready(function(){
 	function ActualizarEstados(){
 		var EstadosUsuario = $("#Estados").val();
 		$("#Estados").value ="";
-		var route = "http://186.64.123.143/estadosempresa";
+		var route = "http://localhost:8000/estadosempresa";
 		var user_id = $("#user_id");
 		var Contador = 0;
 		$.get(route, function(res){
@@ -111,7 +111,7 @@ $(document).ready(function(){
 					"<div id='status' class='list-group'>"
 						+"<div class='list-group-item'>"
 							+"<h4><a href='/profile' style='color:#3C5B28;'>"
-								+"<img class='media-object' src='http://186.64.123.143/images/user.png' data-holder-rendered='true' style='width: 32px; height: 32px;'/>"
+								+"<img class='media-object' src='http://localhost:8000/images/user.png' data-holder-rendered='true' style='width: 32px; height: 32px;'/>"
 								+value.nombre+" "+value.apellido
 							+"</a></h4>"
 							+"<small>"
@@ -133,7 +133,7 @@ $(document).ready(function(){
 
 	function ContarEstados(){
 		var CargarEstados = $("#CargarEstados"); 
-		var route = "http://186.64.123.143/contarestados";
+		var route = "http://localhost:8000/contarestados";
 		var user_id = $("#user_id");
 		var Contador = 0;
 		$.get(route, function(res){
@@ -193,7 +193,7 @@ $(document).ready(function(){
 		var Estados = $("#Estados"); 
 		var empresa = $("#empresa").val();
 		Global_idUltimaPublicacion = $("#idUltima").val();
-		var route = "http://186.64.123.143/estadosempresa/"+Global_idUltimaPublicacion+"/"+empresa;
+		var route = "http://localhost:8000/estadosempresa/"+Global_idUltimaPublicacion+"/"+empresa;
 		var user_id = $("#user_id").val();
 		var empresa_id = $("#empresa_id");
 		var Contador = 0;
@@ -291,7 +291,7 @@ $(document).ready(function(){
 
 	function ContarEstados(){
 		var CargarEstados = $("#CargarEstados"); 
-		var route = "http://186.64.123.143/contarestados";
+		var route = "http://localhost:8000/contarestados";
 		var user_id = $("#user_id");
 		var Contador = 0;
 		$.get(route, function(res){
@@ -311,7 +311,7 @@ $(document).ready(function(){
 	}
 	function ContarCoins(){
 		var CargarEstados = $("#CargarEstados");
-		var route = "http://186.64.123.143/contarcoins";
+		var route = "http://localhost:8000/contarcoins";
 		var user_id = $("#user_id");
 		$.get(route, function(res){
 			$(".CantidadCoins").value = "";
@@ -326,7 +326,7 @@ $(document).ready(function(){
 	function ContarNotificaciones(){
 		var user_id = $("#user_id").val();
 		$.ajax({
-			url: "http://186.64.123.143/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
+			url: "http://localhost:8000/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
 			type: 'GET',
 			dataType: 'json',
 			cache: false,
@@ -351,7 +351,7 @@ $(document).ready(function(){
 		var status_id = valor.replace('estado_','');
 		var user_id = $("#user_id").val();
 		var token = $("#token").val();
-		var route = "http://186.64.123.143/interactuar";
+		var route = "http://localhost:8000/interactuar";
 		$.ajax({
 			url: route,
 			headers: {'X-CSRF-TOKEN': token},
@@ -376,7 +376,7 @@ $(document).ready(function(){
 	}
 	function ContarInteracciones(status_id){
 		status_id = status_id;
-		var route = "http://186.64.123.143/contarinteracciones/"+status_id;
+		var route = "http://localhost:8000/contarinteracciones/"+status_id;
 		var user_id = $("#user_id").val();
 		var Contador = 0;
 		$.get(route, function(res){
