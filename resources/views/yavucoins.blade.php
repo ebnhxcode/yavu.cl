dafd2809368c7298a6033b41a05a403616a8ccee
Luis Zepeda <panic@MacbookPro.home>
2016-06-13 16:02:03 -0400

    Comentarios en cierres de div, a, secction etc

--- resources/views/yavucoins.blade.php
+++ resources/views/yavucoins.blade.php
@@ -9,74 +9,59 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    @section('favicon') {!!Html::favicon('favicons/changeFaviconNameHere.png')!!} @stop
-    @section('title') YavuCoins @stop
-
-
     <!-- Bootstrap Core CSS -->
-       {!!Html::style('css/bootstrap.min.css')!!}
-
-
+    {!!Html::style('css/bootstrap.min.css')!!}
     <!-- Custom Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
     {!!Html::style('font-awesome/css/font-awesome.min.css')!!}
-
     <!-- Plugin CSS -->
      {!!Html::style('css/magnific-popup.css')!!}
-
     <!-- Custom CSS -->
     {!!Html::style('css/creative.css')!!}
 
-
-</head>
+</head> <!-- /head -->
 
 <body id="page-top">
 
     <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand page-scroll" href="/">Inicio</a>
-            </div>
+                </button> <!-- /button -->
+                <a class="navbar-brand page-scroll" href="/">Inicio</a> <!-- /a navbar-brand page-scroll -->
+            </div> <!-- /div container-fluid -->
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-
                     <li>
                         <a class="page-scroll" href="#page-top">Arriba</a>
-                    </li>
+                    </li> <!-- /li -->
                     <li>
-                        <a class="page-scroll" href="#about">Qué son?</a>
-                    </li>
+                        <a class="page-scroll" href="#thatare">Qué son?</a>
+                    </li> <!-- /li -->
                     <li>
-                        <a class="page-scroll" href="#services">Cómo funcionan?</a>
-                    </li>
+                        <a class="page-scroll" href="#howto">Cómo funcionan?</a>
+                    </li> <!-- /li -->
                     <li>
-                        <a class="page-scroll" href="#portfolio">Empresas Asociadas</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Registrate</a>
-                    </li>
-                </ul>
-            </div>
+                        <a class="page-scroll" href="#empresas">Empresas Asociadas</a>
+                    </li> <!-- /li -->
+                    <li> 
+                        <a class="page-scroll" href="#registro">Registrate</a>
+                    </li> <!-- /li -->
+                </ul> <!-- /ul nav navbar-nav navbar-right -->
+            </div> <!-- /div collapse navbar-collapse -->
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container-fluid -->
     </nav>
-
-    <header>
- 
-    </header>
-
-    <section class="bg-primary" id="about">
+    <header> 
+   </header>
+    <section class="bg-primary" id="thatare">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 text-center">
@@ -95,20 +80,20 @@
                     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                    
-                </div>
-            </div>
-        </div>
-    </section>
+                </div> <!-- /div col-lg-8 col-lg-offset-2  -->
+            </div> <!-- /div .row-->
+        </div> <!--/div .container -->
+    </section> <!-- /section #thatare -->
 
-    <section id="services">
+    <section id="howto">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Cómo funcionan?</h2>
                     <hr class="primary">
-                </div>
-            </div>
-        </div>
+                </div> <!-- /div col-lg-12 -->
+            </div> <!-- /div row -->
+        </div> <!-- /div container -->
         <div class="container">
             <div class="row">
                 <div class='col-md-6 col-sm-12 col-xs-12' align='middle'>
@@ -116,20 +101,20 @@
                         <img src="/img/newGraphics/neo_icono_comercio02.png" class="img-responsive">
                         <h3>Acumula tus coins</h3>
                         <p class="text-muted">Acumula nuestras Yavü Coins en las publicaciones de las empresas.</p>
-                    </div>
-                </div>
+                    </div> <!-- /div service-box -->
+                </div> <!-- /div col-md-6 col-sm-12 col-xs-12 -->
                 <div class='col-md-6 col-sm-12 col-xs-12' align='middle'>
                     <div class="service-box">
                         <img src="/img/newGraphics/neo_icono_conoce_empresa.png" class="img-responsive">
                         <h3>Participa por sorteos</h3>
                         <p class="text-muted">Compra Tickets con tus Coins y úsalos en los sorteos.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+                    </div> <!-- /div service-box -->
+                </div> <!-- /div col-md-6 col-sm-12 col-xs-12 -->
+            </div> <!-- /div row -->
+        </div> <!-- /div container -->
+    </section> <!-- /section #howto -->
 
-    <section class="no-padding" id="portfolio">
+    <section class="no-padding" id="empresas">
         <div class="container-fluid">
             <div class="row no-gutter popup-gallery">
                 <div class="col-lg-4 col-sm-6">
@@ -139,14 +124,14 @@
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
                                     Empresa 1
-                                </div>
+                                </div> <!-- /div project-category text-faded -->
                                 <div class="project-name">
                                     Nombre Empresa 1
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                                </div> <!-- /div .project-name-->
+                            </div> <!-- /div .portfolio-box-caption-content -->
+                        </div> <!-- /div .portfolio-box-caption -->
+                    </a> <!-- /a .portfolio-box -->
+                </div> <!-- /div col-lg-4 col-sm-6 -->
                 <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/fullsize/2.jpg" class="portfolio-box">
                         <img src="img/portfolio/thumbnails/2.jpg" class="img-responsive" alt="">
@@ -154,14 +139,14 @@
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
                                     Empresa 2
-                                </div>
+                                </div> <!-- /div project-category text-faded -->
                                 <div class="project-name">
                                     Nombre Empresa 2
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                                </div> <!-- /div .project-name-->
+                            </div> <!-- /div .portfolio-box-caption-content -->
+                        </div> <!-- /div .portfolio-box-caption -->
+                    </a> <!-- /a .portfolio-box -->
+                </div> <!-- /div col-lg-4 col-sm-6 -->
                 <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/fullsize/3.jpg" class="portfolio-box">
                         <img src="img/portfolio/thumbnails/3.jpg" class="img-responsive" alt="">
@@ -169,14 +154,14 @@
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
                                     Empresa 3
-                                </div>
+                                </div> <!-- /div project-category text-faded -->
                                 <div class="project-name">
                                     Nombre Empresa 3
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                                </div> <!-- /div .project-name-->
+                            </div> <!-- /div .portfolio-box-caption-content -->
+                        </div> <!-- /div .portfolio-box-caption -->
+                    </a> <!-- /a .portfolio-box -->
+                </div> <!-- /div col-lg-4 col-sm-6 -->
                 <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/fullsize/4.jpg" class="portfolio-box">
                         <img src="img/portfolio/thumbnails/4.jpg" class="img-responsive" alt="">
@@ -184,14 +169,14 @@
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
                                     Empresa 4
-                                </div>
+                                </div> <!-- /div project-category text-faded -->
                                 <div class="project-name">
                                     Nombre Empresa 4
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                                </div> <!-- /div .project-name-->
+                            </div> <!-- /div .portfolio-box-caption-content -->
+                        </div> <!-- /div .portfolio-box-caption -->
+                    </a> <!-- /a .portfolio-box -->
+                </div> <!-- /div col-lg-4 col-sm-6 -->
                 <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/fullsize/5.jpg" class="portfolio-box">
                         <img src="img/portfolio/thumbnails/5.jpg" class="img-responsive" alt="">
@@ -199,14 +184,14 @@
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
                                     Empresa 5
-                                </div>
+                                </div> <!-- /div project-category text-faded -->
                                 <div class="project-name">
                                     Nombre empresa 5
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                                </div> <!-- /div .project-name-->
+                            </div> <!-- /div .portfolio-box-caption-content -->
+                        </div> <!-- /div .portfolio-box-caption -->
+                    </a> <!-- /a .portfolio-box -->
+                </div> <!-- /div col-lg-4 col-sm-6 -->
                 <div class="col-lg-4 col-sm-6">
                     <a href="img/portfolio/fullsize/6.jpg" class="portfolio-box">
                         <img src="img/portfolio/thumbnails/6.jpg" class="img-responsive" alt="">
@@ -214,44 +199,44 @@
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
                                     Empresa 6
-                                </div>
+                                </div> <!-- /div project-category text-faded -->
                                 <div class="project-name">
                                     Nombre Empresa 6
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
+                                </div> <!-- /div .project-name-->
+                            </div><!-- /div .portfolio-box-caption-content-->
+                        </div> <!-- /div .portfolio-box-caption -->
+                    </a> <!-- /a .portfolio-box -->
+                </div> <!-- /div col-lg-4 col-sm-6 -->
+            </div> <!-- /div .row .no-gutter .popup-gallery -->
+        </div> <!-- /div .container-fluid -->
+    </section> <!-- /section #empresas-->
 
+<section id="registro">
     <aside class="bg-primary">
         <div class="container text-center">
             <div class="call-to-action">
-                <h2>Regístrate Hoy!</h2>
-                <a href="/login" class="btn btn-default btn-xl sr-button">Click Acá</a>
-            </div>
-        </div>
-    </aside>
-
-   
+                
+                <a href="/login" class="btn sr-button">
+                <img src="img/newGraphics/registrate_02a.png" class="img-responsive" alt="">
+                </a> <!-- /a btn sr-button -->
+            </div> <!-- /div .call-to-acction -->
+        </div> <!-- /aside .bg-primary -->
+    </aside> <!-- /aside .bg-primary -->
+ </section> <!-- /section #registro -->
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
     <!-- Plugin JavaScript -->
     <script src="js/scrollreveal.min.js"></script>
     <script src="js/jquery.easing.min.js"></script>
     <script src="js/jquery.fittext.js"></script>
     <script src="js/jquery.magnific-popup.min.js"></script>
-
     <!-- Custom Theme JavaScript -->
     <script src="js/creative.js"></script>
 
-</body>
 
+
+</body> <!-- /body #page-top -->
 </html>
