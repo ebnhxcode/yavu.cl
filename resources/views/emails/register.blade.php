10f66c2ab925f2b3afb1b1d36aa6ef9c6429c47c
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-06-15 13:29:18 -0400

    FIX - Cambios necesarios para local

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -3,7 +3,7 @@
 <body>
 <h1>¡Hola <strong>{!!$nombre!!}</strong>, bienvenido a la comunidad Yavü.</h1>
 	<strong>En Yavu.cl podrás encontrar promociones, descuentos, concursos y eventos de los comercios de tu ciudad. Junta nuestras Yavü Coins en las publicaciones de las empresas, úsalas comprando tickets y utilízalos  para participar de los sorteos!. </strong>
-	<p><strong>Para validar tu cuenta haz click en el enlace de validación: </strong><a href="http://186.64.123.143/verificarusuario/{!! $codigo !!}"> Abrir </a>  </p>
+	<p><strong>Para validar tu cuenta haz click en el enlace de validación: </strong><a href="http://localhost:8000/verificarusuario/{!! $codigo !!}"> Abrir </a>  </p>
 	<h3>¡Mucha suerte! y nos vemos en Yavü.</h3>
 		<h3>Equipo Yavu.cl</h3>
 	</body>
