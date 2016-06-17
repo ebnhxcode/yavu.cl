dfd7a924a5940fdd403d37e11ea7f3c608440cd1
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-06-17 01:14:39 -0400

    FIX - Cambios necesarios para vps test (http://186.64.123.143/)

--- resources/views/feeds/index.blade.php
+++ resources/views/feeds/index.blade.php
@@ -64,7 +64,7 @@
 
 
 		if( user_anon == user_id){
-			var route = "http://localhost:8000/eliminarfeed/"+id;
+			var route = "http://186.64.123.143/eliminarfeed/"+id;
 			$.ajax({
 				url: route,
 				type: 'GET',
@@ -86,7 +86,7 @@
 		var e_id = $('#estado_' + status_id).attr('value').replace('e','');
 		var user_id = $("#user_id").val();
 		var token = $("#token").val();
-		var route = "http://localhost:8000/interactuar";
+		var route = "http://186.64.123.143/interactuar";
 		$.ajax({
 			url: route,
 			headers: {'X-CSRF-TOKEN': token},
@@ -112,7 +112,7 @@
 
 	function ContarInteracciones(status_id){
 		status_id = status_id;
-		var route = "http://localhost:8000/contarinteracciones/"+status_id;
+		var route = "http://186.64.123.143/contarinteracciones/"+status_id;
 		var user_id = $("#user_id").val();
 		var Contador = 0;
 		$.get(route, function(res){
@@ -133,7 +133,7 @@
 	function ContarNotificaciones(){
 		var user_id = $("#user_id").val();
 		$.ajax({
-			url: "http://localhost:8000/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
+			url: "http://186.64.123.143/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
 			type: 'GET',
 			dataType: 'json',
 			cache: false,
@@ -154,7 +154,7 @@
 		return true;
 	}
 	function ContarCoins(){
-		var route = "http://localhost:8000/contarcoins";
+		var route = "http://186.64.123.143/contarcoins";
 		var user_id = $("#user_id");
 		$.get(route, function(res){
 			$(".CantidadCoins").text("");
