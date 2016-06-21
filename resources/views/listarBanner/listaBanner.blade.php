591c161a8a108252d23ea560aa8fc0d951b502ac
Esteban r <esteban.ramos.f@gmail.com>
2016-06-21 00:30:07 -0400

    Respaldo, me estaba yendo por la master

--- resources/views/listarBanner/listaBanner.blade.php
+++ resources/views/listarBanner/listaBanner.blade.php
@@ -1,28 +1,39 @@
 <!-- Banner Random -->
 <div class="list-group">
+  <div class="list-group-item">
+    <span class="glyphicon glyphicon-star"></span>
+    <span style="font-size: 1.8em;" class="glyphicon glyphicon-star"></span>
+    Empresas m&aacute;s seguidas
+  </div>
   <div style="padding: 30px 30px 5px 30px;" class="list-group-item">
     <div class="row">
       <div id="EmpresaListThumbBanner">
         <div class="row">
           @foreach($bannersRandom as $banner)
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
-              <img id="ImagenPortada" src="{!! ($banner->banner!="")?'/img/users/'.$banner->banner:"/img/users/banner.png" !!}" alt="..." style="height: 170px;">
-              <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
-                  @foreach($banner->linksBannerData as $lbd)
-                    <address>
-                      <h4>
+              <div class="thumbnail">
+                <img id="ImagenPortada" src="{!! ($banner->banner!="")?'/img/users/'.$banner->banner:"/img/users/banner.png" !!}" alt="..." style="height: 170px;">
+
+                <div class="caption">
+
+                  <p>
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                      @foreach($banner->linksBannerData as $lbd)
                         <a class="btn-link" href="/empresa/{!!$lbd->link!!}">
                           {!! $lbd->titulo_link!!}
                         </a><!-- /div .btn-link -->
-                      </h4>
-                    </address>
-                  @endforeach
-                  <h6>{!! $banner->descripcion_banner !!}</h6>
+                      @endforeach
+                      <h6>{!! $banner->descripcion_banner !!}</h6>
 
-                </div><!-- /div .col-md12-sm12-xs12 -->
-              </div><!-- /div .row -->
+                    </div><!-- /div .col-md12-sm12-xs12 -->
+                  </div><!-- /div .row -->    
+                  </p>
+
+                </div>
+              </div>
             </div><!-- /div .col-md12-sm12-xs12 -->
             <hr>
           @endforeach
@@ -30,5 +41,6 @@
       </div> <!-- /div  -->
     </div><!-- /div .row -->
   </div> <!-- /div .list-group-item styled -->
+  <small>Ads via <a class="btn-link" href="http://yavu.cl/">Yavü</a></small>
 </div><!-- /div .list-group -->
 
