aadf1ac9a3f6cd0e9acb9d1eaa5e9e84d5f6ca7a
Esteban r <esteban.ramos.f@gmail.com>
2016-06-24 19:48:24 -0400

    FIX - Cambios necesarios VPS

--- resources/views/feeds/indexPartial/sectionRight.blade.php
+++ resources/views/feeds/indexPartial/sectionRight.blade.php
@@ -67,6 +67,6 @@
       </div> <!-- /div  -->
     </div><!-- /div .row -->
   </div> <!-- /div .list-group-item styled -->
-  <small>Ads via <a class="btn-link" href="http://localhost:8000/">Yavü</a></small>
+  <small>Ads via <a class="btn-link" href="http://186.64.123.143/">Yavü</a></small>
 </div><!-- /div .list-group -->
 
