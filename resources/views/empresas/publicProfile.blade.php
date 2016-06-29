90d2956db2d2aa40787336e2c26b84bcddf80122
Esteban r <esteban.ramos.f@gmail.com>
2016-06-29 19:06:31 -0400

    FIX - Cambios necesarios vps test

--- resources/views/empresas/publicProfile.blade.php
+++ resources/views/empresas/publicProfile.blade.php
@@ -216,7 +216,7 @@
   function ContarNotificaciones(){
     var user_id = $("#user_id").val();
     $.ajax({
-      url: "http://localhost:8000/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
+      url: "http://186.64.123.143/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
       type: 'GET',
       dataType: 'json',
       cache: false,
@@ -237,7 +237,7 @@
     return true;
   }
   function ContarCoins(){
-    var route = "http://localhost:8000/contarcoins";
+    var route = "http://186.64.123.143/contarcoins";
     var user_id = $("#user_id");
     $.get(route, function(res){
       $(".CantidadCoins").text("");
@@ -256,7 +256,7 @@
 
 
     if( user_anon == user_id){
-      var route = "http://localhost:8000/eliminarfeed/"+id;
+      var route = "http://186.64.123.143/eliminarfeed/"+id;
       $.ajax({
         url: route,
         type: 'GET',
@@ -278,7 +278,7 @@
     var user_id = $("#user_id").val();
     var e_id = $('#estado_' + status_id).attr('value').replace('e','');
     var token = $("#token").val();
-    var route = "http://localhost:8000/interactuar";
+    var route = "http://186.64.123.143/interactuar";
     $.ajax({
       url: route,
       headers: {'X-CSRF-TOKEN': token},
@@ -303,7 +303,7 @@
   }
   function ContarInteracciones(status_id){
     status_id = status_id;
-    var route = "http://localhost:8000/contarinteracciones/"+status_id;
+    var route = "http://186.64.123.143/contarinteracciones/"+status_id;
     var user_id = $("#user_id").val();
     var Contador = 0;
     $.get(route, function(res){
