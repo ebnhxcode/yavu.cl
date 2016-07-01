1d65d39d57d472e2f4e56ddc1d36d847bd45b8cd
Esteban r <esteban.ramos.f@gmail.com>
2016-07-01 06:01:34 -0400

    ADD - Se agrego el boton de estadisticas en el dashboard

--- resources/views/usuarios/dashboard.blade.php
+++ resources/views/usuarios/dashboard.blade.php
@@ -51,6 +51,18 @@
             </div><!-- /div .list-group -->
           </div><!-- /div .col-md4-sm6-xs12 -->
 
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class='list-group' >
+              <a style="text-align:center;" data-toggle="modal" data-target="#myModal" class="list-group-item list-group-item-info btn">
+                <span>
+                  <h4>INFORMES</h4>
+                  <img  src="{!!URL::to('img/newGraphics/neo_icono_informe.png')!!}" class=''/>
+                </span>
+              </a><!-- /a .list-group-item .info styled -->
+            </div><!-- /div .list-group -->
+          </div><!-- /div .col-md4-sm-4-xs6 -->
+
+          {{--
 					<div class='col-md-4 col-sm-6 col-xs-12'>
 						<div class='list-group' >
 							<a href='{!!URL::to('/tickets/history')!!}' style='text-align:center;' class='list-group-item list-group-item-info'>
@@ -61,8 +73,9 @@
               </a><!-- /a .list-group-item .success -->
             </div><!-- /div .list-group -->
           </div><!-- /div .col-md4-sm6-xs12 -->
+          --}}
 
-          <div id="sorteo" class='col-md-4 col-sm-6 col-xs-12' style="display: none;">
+          <div id="sorteo" class='col-md-4 col-sm-6 col-xs-12' {{--style="display: none;"--}}>
             <div class='list-group' >
               <a href='{!!URL::to('/sorteos')!!}' style='text-align:center;' class='list-group-item list-group-item-warning'>
                 <span>
@@ -84,13 +97,73 @@
             </div><!-- /div .list-group -->
           </div><!-- /div .col-md4-sm6-xs12 -->
 
-
-
 				</div><!-- /div .row -->
 			</div><!-- /div col-md9-sm9-xs12 -->
 		</div><!-- /div .row -->
 	</div><!-- /div #fullwidth -->
 </div><!-- /div .jumbotron -->
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Seleccione tipo de informe que desea ver</h4>
+      </div><!-- /div .modal-header -->
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <div class='list-group' >
+              <div align="center">
+                <a style="padding: 2px 2px 2px 2px;" href='{!!URL::to('/coins/history')!!}' style="text-align:center;" class="list-group-item list-group-item-info">
+                <span>
+                  <img width="80%" src= "{!!URL::to('img/newGraphics/yavucoin_neo02a.png')!!}"/>
+                </span>
+                </a><!-- /a .list-group-item .success -->
+              </div><!-- /div aligned -->
+              <div align="center">
+                <small>Informe de Coins</small>
+              </div><!-- /div aligned -->
+            </div><!-- /div .list-group -->
+          </div><!-- /div .col-md4-sm4-xs4 -->
+          <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <div class="list-group">
+              <div align="center">
+                <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/tickets/history')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
+                  <span>
+                    <img style="padding-bottom: 10px;" width="80%"src= "{!!URL::to('img/newGraphics/neo_icono_tickets.png')!!}"/>
+                  </span>
+                </a>
+              </div><!-- /div aligned -->
+              <div align="center">
+                <small>Informe de Tickets</small>
+              </div><!-- /div aligned -->
+              <br>
+            </div><!-- /div .list-group -->
+          </div><!-- /div .col-md4-sm4-xs4 -->
+          <div style="display: none;" class="col-xs-4 col-sm-4 col-md-4 col-lg-4 hasoneempresa">
+            <div class="list-group" >
+              <div align="center">
+                <a style="padding: 2px 2px 2px 2px;" href="{!!URL::to('/estadisticasdemiempresa')!!}" style="text-align:center;" class="list-group-item list-group-item-info">
+                  <span>
+                    <img width="80%" src= "{!!URL::to('img/dash/icono_informe01.png')!!}"/>
+                  </span>
+                </a>
+              </div><!-- /div aligned -->
+              <div align="center">
+                <small>Estad&iacute;sticas de mi empresa</small>
+              </div><!-- /div aligned -->
+            </div><!-- /div .list-group -->
+          </div><!-- /div .col-md4-sm4-xs4 -->
+        </div><!-- /div .row -->
+      </div><!-- /div .modal-body -->
+      <div class="modal-footer">
+      </div><!-- /div .modal-footer -->
+    </div><!-- /div .modal-content -->
+  </div><!-- /div .modal-dialog -->
+</div><!-- /div .modal-fade -->
+
 @stop
 
 
