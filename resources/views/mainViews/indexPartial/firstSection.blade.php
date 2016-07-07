5c2e44ad8f9653ee09f555a143d27b3c57934275
Ignacio Rojas <ignacio.rojas01@gmail.com>
2016-07-07 10:31:48 -0400

    Backup index blade partials y css

--- resources/views/mainViews/indexPartial/firstSection.blade.php
+++ resources/views/mainViews/indexPartial/firstSection.blade.php
@@ -1,18 +1,15 @@
-<div align="center" class="row" style="padding-bottom: 80px; padding-top: 80px; background-color: #6BA410;">
-  <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-    <h2>
-      Yavü es una plataforma en donde empresas, concursos y premios coexisten para brindar un servicio único a nuestros usuarios.
-    </h2>
-  </div><!-- /div .col-lg4-md4-sm12-xs12 -->
+<div align="center" class="row">
 
   <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-    <img src="/img/icono_que_es.png" alt=""/>
-  </div><!-- /div .col-lg4-md4-sm12-xs12 -->
+    <img class="img-responsive" src="/img/newGraphics/icono_mac01.png" alt="">
+  </div><!-- /div .col-lg12-md12-sm12-xs12 -->
 
-  <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-    <h3>
-      Some text
-    </h3>
-  </div><!-- /div .col-lg4-md4-sm12-xs12 -->
-
+  <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+    <div class="panel-body">
+      <h3>¿Que es Yavü?</h3>
+      <h4>
+        Yavü es la plataforma en donde empresas, concursos y premios coexisten para brindar un servicio único a nuestros usuarios. Sigue a comercios de tu interés, conoce promociones y descuentos en tu ciudad, participa en concursos todas las semanas y entérate de los eventos más importantes totalmente gratis en Yavu.cl.
+      </h4>
+    </div><!-- /div .panel-body -->
+  </div><!-- /div .col-lg12-md12-sm12-xs12 -->
 </div><!-- /div .row -->
--- resources/views/mainViews/indexPartial/fourthSection.blade.php
+++ resources/views/mainViews/indexPartial/fourthSection.blade.php
@@ -13,38 +13,13 @@
     </div><!-- /div .panel-body -->
   </div><!-- /div .col-lg12-md12-sm12-xs12 -->
 
+  <!--
   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     <img src="/img/cards/barra_separadora.png" class="img-responsive">
     <br>
-  </div>
+  </div> -->
 
-  <div class='col-xs-12 col-sm-12 col-md-4 col-lg-4' align='middle'>
-    <img src="/img/newGraphics/neo_icono_premios.png" class="img-responsive">
-    <div class="panel-body">
-      <p class='lead'>Descubre comercios en tu ciudad y participa por premios.</p>
-    </div><!-- /div .panel-body -->
-  </div><!-- /div .col-md4-sm12-xs12 aligned -->
 
-  <div class='col-xs-12 col-sm-12 col-md-4 col-lg-4' align='middle'>
-    <img src="/img/newGraphics/neo_icono_comercio02.png" class="img-responsive">
-    <div class="panel-body">
-      <p class='lead'>Acumula nuestras Yavü Coins en las publicaciones de las empresas.</p>
-    </div><!-- /div .panel-body -->
-  </div><!-- /div .col-md4-sm12-xs12 aligned -->
-
-  <div class='col-xs-12 col-sm-12 col-md-4 col-lg-4' align='middle'>
-    <img src="/img/newGraphics/neo_icono_conoce_empresa.png" class="img-responsive">
-    <div class="panel-body">
-      <p class='lead'>Compra Tickets con tus Coins y úsalos en los sorteos.</p>
-    </div><!-- /div .panel-body -->
-  </div><!-- /div .col-md4-sm12-xs12 aligned -->
-
-  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" align="middle">
-    <a data-toggle="modal" data-target="#gridSystemModal" role="button" href="#!">
-      <img src="img/newGraphics/registrate_02a.png" class="img-responsive" />
-    </a>
-    <br>
-  </div><!-- /div .col-md12-sm12-xs12 aligned -->
 
 </div><!-- /div .row -->
 
