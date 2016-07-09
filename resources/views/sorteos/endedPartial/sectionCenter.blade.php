e11c6ae0405420ca6d6c2d3a18176247dca1b88d
Esteban r <esteban.ramos.f@gmail.com>
2016-07-09 17:36:38 -0400

    ADD - List group con enlace a sorteos finalizados

--- resources/views/sorteos/endedPartial/sectionCenter.blade.php
+++ resources/views/sorteos/endedPartial/sectionCenter.blade.php
@@ -6,23 +6,21 @@
         <div class="list-group-item">
           <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1">
-              <img class='media-object' src='/img/users/{!! ($companyProfileImage = $sorteo->companyAuthorRaffle->imagen_perfil)?$companyProfileImage:'usuario_nuevo.png' !!}' data-holder-rendered='true' style='width: 48px; height: 48px; border-radius: 10%; float:left;'/>
+              <a href="/empresas/{!! $sorteo->empresa_id !!}">
+                <img data-toggle="tooltip" data-placement="top" title="{!! $sorteo->nombre_empresa !!}" class='media-object' src='/img/users/{!! ($companyProfileImage = $sorteo->companyAuthorRaffle->imagen_perfil)?$companyProfileImage:'usuario_nuevo.png' !!}' data-holder-rendered='true' style='width: 48px; height: 48px; border-radius: 10%; float:left;'/>
+              </a>
             </div><!-- /div .col-xs12-sm12-md1-lg1 -->
             <div class="col-xs-12 col-sm-12 col-md-11 col-lg-11">
               <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
 
-
-                  <div style="float:right;">
-                    <small><a href="{!! URL::to('/empresas/'.$sorteo->empresa_id) !!}">{!! $sorteo->nombre_empresa !!}</a><br></small>
-                  </div><br>
                   <div style="float:right;">
 
                     <div style="z-index: auto;" class="dropup">
                       <button class="btn btn-default btn-xs dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="glyphicon glyphicon-chevron-down"></span>
                       </button><!-- /div .btn .btn-default .btn-xs .dropdown-toggle -->
-                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                      <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2">
                         <li><a href="{!! URL::to('empresa/'.$sorteo->nombre_empresa.'/sorteos') !!}">Ver m&aacute;s sorteos de {!! $sorteo->nombre_empresa !!}</a></li>
                         <li>{!!link_to_route('sorteos.show', $title = 'Ver mas detalles', $parameters = $sorteo->id, $attributes = [])!!}</li>
                         @if(Auth::user()->get()->id == $sorteo->user_id && $sorteo->estado_sorteo == 'Activo')
@@ -39,24 +37,16 @@
 
 
                   <div>
-                    <h3><b><a style="padding-left: 15px;" href="/sorteos/{!! $sorteo->id !!}"><span class="text-info">{!!$sorteo->nombre_sorteo!!}</span><br></a></b></h3>
+                    <h3><b><a style="padding-left: 5px;" href="/sorteos/{!! $sorteo->id !!}"><span>{!!$sorteo->nombre_sorteo!!}</span><br></a></b></h3>
                   </div>
 
 
-                  <div class="amplio">
-                    <a style="padding-left: 15px;" href="/sorteos/{!! $sorteo->id !!}"><span class="text-info">{!!$sorteo->descripcion!!}</span><br></a>
-                  </div>
-
-                  <div class="amplio softText-descriptions-middle">
-                    <a style="padding-left: 15px;" href="/sorteos/{!! $sorteo->id !!}">
-                      <small> Este sorteo se realizara dentro de
-                        <abbr class='timeago text-danger' id='timeago{!! $sorteo->id !!}' value='{!! $sorteo->fecha_inicio_sorteo !!}' title='{!! $sorteo->fecha_inicio_sorteo !!}'>{!! $sorteo->fecha_inicio_sorteo !!}</abbr>
-                      </small>
-                    </a>
+                  <div class="">
+                    <a style="padding-left: 5px;" href="/sorteos/{!! $sorteo->id !!}"><span>{!!$sorteo->descripcion!!}</span><br></a>
                   </div>
 
                   <div class="">
-                    <button class="btn btn-success btn-xs UsarTicket" value="{!! $sorteo->id !!}" type="button"   style="display: none;" data-dismiss="modal">PARTICIPAR</button>
+                    <button class="btn btn-success btn-sm UsarTicket" value="{!! $sorteo->id !!}" type="button"   style="display: none;" data-dismiss="modal">PARTICIPAR</button>
                     <br>
                   </div><!-- /div .semi-amplio -->
 
@@ -89,56 +79,15 @@
                     </div><!-- /div list-group-item -->
 
 
-                    <div class="list-group-item">
-
-
-                      <div style="" class="row">
-                        <div align="center" class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                          <span data-toggle="tooltip" data-placement="top" title="Tickets en juego"  class="glyphicon glyphicon-user"></span><br>
-                          <span class="TicketsEnSorteo" id="{!! $sorteo->id !!}"></span>
-                          <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-                        </div><!-- /div .col-md3-sm3-xs3 -->
-                        <div align="center" class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                          <span data-toggle="tooltip" data-placement="top" title="Mis tickets jugados"  class="glyphicon glyphicon-tag"></span><br>
-                          <span class="MisTicketsUsados" id="{!! $sorteo->id !!}"></span>
-                        </div><!-- /div .col-md3-sm3-xs3 -->
-                        <div align="center" class="col-md-6 col-sm-6 col-xs-6">
-                          <span data-toggle="tooltip" data-placement="top" title="Hora del sorteo"  class="glyphicon glyphicon-time"></span><br>
-                          <small>
-                            <abbr title="{!! $sorteo->fecha_inicio_sorteo !!}">21:00 hrs</abbr>
-                          </small>
-                        </div><!-- /div .col-md6-sm6-xs6 -->
-                      </div> <!-- /div .row -->
-
-
-                    </div><!-- /div .list-group-item -->
-
                   </div><!-- /div .list-group -->
 
 
 
 
-                  <div class="amplio">
-
+                  <div class="softText-descriptions-middle">
+                    Sorteo finalizado <a style="float:right;" class="text-warning" href="/sorteos/{!! $sorteo->id !!}">Ver quien fué el ganador</a>
                   </div><!-- /div .amplio -->
 
-
-                  <div class="row softText-descriptions">
-                    <div align="" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                      <span class="glyphicon glyphicon-user"></span>
-                      <small>Total de tickets en juego</small>
-                      <br>
-                      <span class="glyphicon glyphicon-tag"></span>
-                      <small>Mis tickets usados para este sorteo</small>
-                      <br>
-                      <span class="glyphicon glyphicon-time"></span>
-                      <small>Hora del sorteo</small>
-                      <br>
-                      <small>
-                        <span id="estado"></span><!-- /div #estado -->
-                      </small>
-                    </div><!-- /div .col-md12-sm12-xs12 -->
-                  </div><!-- /div .row -->
 
                 </div> <!-- /div .col-md8-sm12-xs12 -->
               </div><!-- /div .row -->
@@ -157,12 +106,10 @@
       <div>
         {!!$sorteos->render()!!}
       </div><!-- /div -->
-      <small class="text-info">
-        Navega entre p&aacute;ginas para encontrar m&aacute;s sorteos
-      </small><!-- /small .text-info -->
     </div><!-- /div .list-group-item styled -->
 
   </div><!-- /div #SorteoListThumb -->
+
 
 
 </div><!-- /div .list-group -->
