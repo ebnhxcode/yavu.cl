d7f8deb5d2f7d91b0a59daced49b0db5ff7c69b9
Esteban r <esteban.ramos.f@gmail.com>
2016-07-15 11:59:15 -0400

    UPDATE - Revision y orden de codigo en vista sectionLeft.blade.php de publicProfile

--- resources/views/empresas/publicProfilePartial/sectionLeft.blade.php
+++ resources/views/empresas/publicProfilePartial/sectionLeft.blade.php
@@ -1,135 +1,84 @@
 <div class="row">
   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
     <div class="list-group">
-      <div class="list-group-item-full-header">
-
+      <div class="list-group-item">
         <div class="thumbnail">
-
-
-          @if($e->imagen_perfil === "")
-            <img id="ImagenPerfil" src="/img/users/usuario_nuevo.png" class="center-block" class="thumbnail">
+          <img id='ImagenPerfil' src='/img/users/{!! isset($e)?($e->imagen_perfil!='')?$e->imagen_perfil:'usuario_nuevo.png':'' !!}' class="center-block" class="thumbnail" class="img-rounded" width="300px" height="100px" class="img-responsive">
+          {!! Form::hidden('company_id', $e->id, ['id' => 'company_id']) !!}
+        </div><!-- /div .thumbnail -->
+        <div class="caption">
+          @if(Auth::user()->check())
+            <div class="list-group">
+              {!!Form::hidden('empresa', $e->nombre, ['id'=>'empresa'])!!}
+              <span class="btn btn-primary btn-sm seguir" value="{!! $e->id !!}" role="button">{!! count($e->isFollowedBy($userSession->id))>0?'Siguiendo':'Seguir' !!}</span>
+              <input type="text" class="btn btn-sm text-success" id="seguidores{!! $e->id !!}" size="10" disabled value="{!! $e->followers()->count('id') !!}" >
+              <div style="float: right;">
+                @if($e->user_id == Auth::user()->get()->id)
+                  <div class="dropdown">
+                    <button class="btn btn-default btn-sm dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                      <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                      <span class="caret"></span>
+                    </button><!-- /div .btn .btn-default .btn-sm .dropdown-toggle -->
+                    <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
+                      <li><a href="{!!URL::to('/feeds')!!}">Inicio</a></li>
+                      <li>{!!link_to_route('empresas.edit', $title = 'Editar Perfil de Empresa', $parameters = $e->id, $attributes = [])!!}</li>
+                      <li><a href="{!! route('usuarios_edit_path', Auth::user()->get()->id) !!}">Editar Perfil de Usuario</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="{!!URL::to('/sorteos/create')!!}" style="text-align:center;" class=""><span>Crear sorteo <img width="40%" src= "{!!URL::to('img/dash/ico_sorteo01.png')!!}"/></span></a></li>
+                      <li><a href='{!!URL::to('/feeds')!!}' style="text-align:center;" class=""><span>Publicaciones <img width="40%" src= "{!!URL::to('img/dash/ico_pin03.png')!!}"/></span></a></li>
+                    </ul><!-- /div .dropdown-menu .pull-right -->
+                  </div><!-- /div .dropdown -->
+                @endif
+              </div><!-- /div styled -->
+            </div><!-- /div .list-group -->
           @else
-            <img id="ImagenPerfil" src="/img/users/{!!$e->imagen_perfil!!}" class="center-block" class="thumbnail" class="img-rounded" width="300px" height="100px" class="img-responsive" >
+            <a href="{!! URL::to('/usuarios/create') !!}" class="btn btn-primary btn-sm" role="button">Seguir</a>
+            <input type="text" class="btn btn-sm text-success" id="seguidores" size="10" disabled >
+            <small>Para seguir a esta empresa debes registrarte</small>
           @endif
 
-          {!! Form::hidden('company_id', $e->id, ['id' => 'company_id']) !!}
+          <div style="font-size: 1.2em;">
+            <address>
+              <br>
+              <strong>{!! strtoupper("".$e->nombre)!!}.</strong><br />
+              Direcci&oacute;n : {!!$e->direccion!!}<br>
+              Ciudad : {!!$e->ciudad!!}<br>
+              Fono : <abbr title="Phone"></abbr> {!!$e->fono!!}
+            </address>
 
-          <div class="caption">
-            @if(Auth::user()->check())
-              <div class="list-group">
+            <address>
+              <strong>Contacto: <a href="mailto:#">{!!$e->email!!}</a></strong><br>
+            </address>
 
-                <div>
-                  {!!Form::hidden('empresa', $e->nombre, ['id'=>'empresa'])!!}
-                  <div>
-                    <span class="btn btn-primary btn-sm seguir" value="{!! $e->id !!}" role="button">{!! count($e->isFollowedBy($userSession->id))>0?'Siguiendo':'Seguir' !!}</span>
-                    <input type="text" class="btn btn-sm text-success" id="seguidores{!! $e->id !!}" size="10" disabled value="{!! $e->followers()->count('id') !!}" >
+            <!-- descripcion en acordeon -->
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+              <div class="panel panel-success">
+                <div class="panel-heading" role="tab" id="headingOne">
+                  <h4 class="panel-title"><a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Ver Descripción</a></h4><!-- /div .panel-title -->
+                </div><!-- /div .panel-heading -->
+                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                  <div class="panel-body">
+                    {!! $e->descripcion !!}
+                  </div><!-- /div .panel-body -->
+                </div><!-- /div .collapseOne -->
+              </div><!-- /div .panel .panel-success -->
+            </div><!-- /div .panel-group -->
+            <!-- fin acordeon -->
 
+          </div><!-- /div styled -->
+        </div><!-- /div .caption -->
+      </div><!-- /div .list-group-item -->
+    </div><!-- /div .list-group -->
 
-                    <div style="float: right;" class="">
-                      @if($e->user_id == Auth::user()->get()->id)
-                        <div class="">
-                          <div class="" >
-                            <div>
-                              <div class="dropdown">
-                                <button class="btn btn-default btn-sm dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                  <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-                                  <span class="caret"></span>
-                                </button><!-- /div .btn .btn-default .btn-sm .dropdown-toggle -->
-                                <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
-                                  <li><a href="{!!URL::to('/feeds')!!}">Inicio</a></li>
-                                  <li>
-                                    {!!link_to_route('empresas.edit', $title = 'Editar Perfil de Empresa', $parameters = $e->id, $attributes = [])!!}
-                                  </li>
-                                  <li>
-                                    <a href="{!! route('usuarios_edit_path', Auth::user()->get()->id) !!}">Editar Perfil de Usuario</a>
-                                  </li>
-                                  <li role="separator" class="divider"></li>
-                                  <li>
-                                    <a href="{!!URL::to('/sorteos/create')!!}" style="text-align:center;" class="">
-                                      <span>
-                                        Crear sorteo <img width="40%" src= "{!!URL::to('img/dash/ico_sorteo01.png')!!}"/>
-                                      </span>
-                                    </a>
-                                  </li>
-                                  <li>
-                                    <a href='{!!URL::to('/feeds')!!}' style="text-align:center;" class="">
-                                      <span>
-                                        Publicaciones <img width="40%" src= "{!!URL::to('img/dash/ico_pin03.png')!!}"/>
-                                      </span>
-                                    </a>
-                                  </li>
-                                </ul><!-- /div .dropdown-menu .pull-right -->
-                              </div><!-- /div .dropdown -->
-                            </div>
-                          </div>
-                        </div>
-                      @endif
-                    </div>
+    <!-- gmaps -->
+    <div class="list-group">
+      <div class="list-group-item">
+        @include('empresas.forms.modalModificarDireccionMapa')
+      </div><!-- /div .list-group-item -->
+    </div><!-- /div .list-group -->
+    <!-- /gmaps -->
 
-
-                  </div>
-                  @else
-                    <p>
-                      <a href="{!! URL::to('/usuarios/create') !!}" class="btn btn-primary btn-sm" role="button">Seguir</a>
-                      <input type="text" class="btn btn-sm text-success" id="seguidores" size="10" disabled >
-                    </p>
-                    <small>Para seguir a esta empresa debes registrarte</small>
-                  @endif
-                </div>
-
-                <div style="font-size: 1.2em;">
-                  <address>
-                    <br>
-                    <strong>{!! strtoupper("".$e->nombre)!!}.</strong><br />
-                    Direcci&oacute;n : {!!$e->direccion!!}<br>
-                    Ciudad : {!!$e->ciudad!!}<br>
-                    Fono : <abbr title="Phone"></abbr> {!!$e->fono!!}
-                  </address>
-
-                  <address>
-                    <strong>Contacto: <a href="mailto:#">{!!$e->email!!}</a></strong><br>
-                  </address>
-
-                  <!-- descripcion en acordeon -->
-                  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                    <div class="panel panel-success">
-                      <div class="panel-heading" role="tab" id="headingOne">
-                        <h4 class="panel-title">
-                          <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                            Ver Descripción
-                          </a>
-                        </h4>
-                      </div>
-                      <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                        <div class="panel-body">
-                          {!! $e->descripcion !!}
-                        </div>
-                      </div>
-                    </div>
-                  </div> <!-- fin acordeon -->
-
-                </div>
-
-
-              </div>
-          </div>
-
-        </div>
-
-        <!-- gmaps -->
-        <div class="list-group">
-          <div class="list-group-item">
-            @include('empresas.forms.modalModificarDireccionMapa')
-          </div>
-        </div>
-        <!-- /gmaps -->
-
-      </div>
-    </div>
-  </div><!-- /div col-md-7 col-sm-7 col-xs-7 -->
-
+  </div><!-- /div .col-lg12-md12-sm12-xs12 -->
   <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-
   </div><!-- /div col-xs-5 col-sm-5 col-md-5 col-lg-5 -->
-</div>
+</div><!-- /div .row -->
