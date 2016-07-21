c0ca0d7f9ce23d538b35c2dad2817324b04cadf3
Esteban r <esteban.ramos.f@gmail.com>
2016-07-21 17:41:29 -0400

    FIX

--- resources/views/empresas/indexPartial/sectionCenter.blade.php
+++ resources/views/empresas/indexPartial/sectionCenter.blade.php
@@ -35,7 +35,7 @@
               <strong>Ciudad :</strong> {!!$empresa->ciudad!!}<br>
               <strong>Contacto :<strong><a href="mailto:#">{!!$empresa->email!!}</a></strong><br>
                 <strong>Fono :</strong> <abbr title="Phone"></abbr> {!!$empresa->fono!!}</strong><br>
-              {{($fCounts=round( count($empresa->followers)*(int)("7.".rand(1,9999)) ) )}} seguidor{{($fCounts>1||$fCounts==0?'es':'')}}.<br>
+              {{($fCounts=round( count($empresa->followers)*(int)("3.".rand(1,9999)) ) )}} seguidor{{($fCounts>1||$fCounts==0?'es':'')}}.<br>
               {!! round(count($empresa->visits)*3.6) !!} visitas<br>
 
               {{--
