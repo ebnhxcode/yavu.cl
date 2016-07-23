49f48b630c1e909e428ccd39761a1b7bb8109d39
Esteban r <esteban.ramos.f@gmail.com>
2016-07-23 13:09:50 -0400

    FIX - Cambios  a local

--- public/js/admins/admins.js
+++ public/js/admins/admins.js
@@ -4,7 +4,7 @@
 
 
 function aprobarSorteo(id){
-  var route = "http://yavu.cl/aprobarsorteopendiente";
+  var route = "http://localhost:8000/aprobarsorteopendiente";
   var token = $("#token").val();
   $.ajax({
     url: route,
@@ -20,7 +20,7 @@ function aprobarSorteo(id){
   return true;
 }
 function visualizarEmpresaSorteo(id){
-  var route = "http://yavu.cl/visualizarempresasorteopendiente";
+  var route = "http://localhost:8000/visualizarempresasorteopendiente";
   var token = $("#token").val();
   $.ajax({
     url: route,
