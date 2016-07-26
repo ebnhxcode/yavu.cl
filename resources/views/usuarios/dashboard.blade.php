00dee3c24c0e9c051f8496bb608879f9fe217622
Esteban r <esteban.ramos.f@gmail.com>
2016-07-26 13:13:28 -0400

    fix - YAVU POSTS! image

--- resources/views/usuarios/dashboard.blade.php
+++ resources/views/usuarios/dashboard.blade.php
@@ -28,7 +28,7 @@
               <a href='{!!URL::to('/feeds')!!}' style='text-align:center;' class='list-group-item'>
                 <span>
                   <h4>YAVÜ POSTS!</h4>
-                  <img width="100" class="img-responsive-centered" src="{!!URL::to('img/newGraphics/yavucoin_neo01b.png')!!}" />
+                  <img class="img-responsive-centered" src="{!!URL::to('img/newGraphics/neo_icono_comercio02.png')!!}" />
                 </span>
               </a><!-- /a .list-group-item .success -->
             </div><!-- /div .list-group -->
