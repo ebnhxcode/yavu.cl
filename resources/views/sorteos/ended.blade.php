a144752c18fa3a2012d8618d6c6aabc8fa38cc96
Esteban r <esteban.ramos.f@gmail.com>
2016-07-26 22:50:16 -0400

    update - estructura de formato base para sorteos metodo / vista ended

--- resources/views/sorteos/ended.blade.php
+++ resources/views/sorteos/ended.blade.php
@@ -9,13 +9,13 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           @include('alerts.allAlerts')
         </div><!-- /div col-md12-sm12-xs12 -->
-        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-          @include('sorteos.indexPartial.sectionLeft')
+        <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
+          @include('sorteos.showPartial.sectionLeft')
         </div><!-- /div .col-md4-sm12-xs12 -->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        <div class="col-xs-8 col-sm-6 col-md-6 col-lg-6">
           @include('sorteos.endedPartial.sectionCenter')
         </div><!-- /div .col-md8-sm12-xs12 -->
-        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+        <div class="col-xs-8 col-sm-3 col-md-3 col-lg-3" style="float:right;">
           @include('empresas.indexPartial.sectionRight')
         </div><!-- /div .col-md3-sm12-xs12 -->
       </div><!-- /div .row -->
