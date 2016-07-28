45943f403db42711d16fffb752bc4349362feca4
Esteban r <esteban.ramos.f@gmail.com>
2016-07-28 17:25:00 -0400

    patch - boton

--- resources/views/sorteos/showPartial/sectionLeft.blade.php
+++ resources/views/sorteos/showPartial/sectionLeft.blade.php
@@ -3,22 +3,24 @@
 
     <div class="list-group-item">
       <small>LISTA DE SORTEOS ACTIVOS</small>
+      <a href="/sorteos" style="float: right;">
+        <b class="softText-descriptions">
+          ¡{{count($sorteos)}} Sorteos!
+        </b>
+      </a>
     </div><!-- /div .list-group-item .success -->
 
+    <div class="list-group-item">
+      <div class="collapse" id="collapseExample">
 
-
-      <div class="list-group-item">
-        <div class="collapse" id="collapseExample">
-
-          @foreach($sorteos as $sorteo)
+        @foreach($sorteos as $key => $sorteo)
           <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
               <a href="/sorteos/{!! $sorteo->id !!}"><b>{!! $sorteo->nombre_sorteo !!}</b></a>
 
-
-            <span class="text-info" style="float: right;font-size: 0.8em;">
-              <small><a href="/empresas/{!! $sorteo->empresa_id !!}">{!! $sorteo->nombre_empresa !!}</a></small>
-            </span>
+              <span class="text-info" style="float: right;font-size: 0.8em;">
+                <small><a href="/empresas/{!! $sorteo->empresa_id !!}">{!! $sorteo->nombre_empresa !!}</a></small>
+              </span><!-- /span .text-info -->
 
             </div><!-- /div .col-lg12-md12-sm12-xs12 -->
 
@@ -26,34 +28,33 @@
               <div class="softText-descriptions">
                 {!! (($participants = $sorteo->participants()->count())>0)?$participants:'Sin' !!} opciones en tickets <br>
                 Premiaci&oacute;n dentro de <abbr class='timeago text-danger' id='timeago{!! $sorteo->id !!}' value='{!! $sorteo->fecha_inicio_sorteo !!}' title='{!! $sorteo->fecha_inicio_sorteo !!}'>{!! $sorteo->fecha_inicio_sorteo !!}</abbr>
-              </div>
+              </div><!-- /div .softText-descriptions -->
             </div><!-- /div .col-lg12-md12-sm12-xs12 -->
+            
             <hr>
+
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <a style="float:right;" class="btn btn-success btn-xs" style="" href="{!!URL::to('/sorteos/'.$sorteo->id)!!}">
+              <a style="float:right;" class="btn btn-success btn-xs" href="{!!URL::to('/sorteos/'.$sorteo->id)!!}">
                 <img src="{!! asset('img/yavu019.png') !!}" width="20" alt=""/>
-                ¡<small>Ir a ver el sorteo</small>!
-              </a>
+                ¡<small>IR A VER EL SORTEO</small>!
+              </a><!-- /div .btn .btn-success .btn-xs -->
             </div><!-- /div .col-lg12-md12-sm12-xs12 -->
           </div><!-- /div .row -->
-            <hr>
-          @endforeach
+          <hr>
+        @endforeach
 
-
-
-        </div><!-- /div #collapseExample .collapse -->
-          <small><span id="openclose" data-toggle="collapse" data-target="#collapseExample" class="btn btn-default btn-xs">ABRIR</span></small>
-        <script>
-          $('#openclose').click(function(){
-            if($(this).text() == 'ABRIR') $(this).text('CERRAR');
-            else if ($(this).text() == 'CERRAR') $(this).text('ABRIR');
-          });
-        </script>
-      </div><!-- /div .list-group-item -->
-
-
-
-
-
+      </div><!-- /div #collapseExample .collapse -->
+      <small>
+        <span id="openclose" data-toggle="collapse" data-target="#collapseExample" class="btn btn-default btn-xs">
+          ABRIR
+        </span><!-- #openclose .btn .btn-default .btn-xs -->
+      </small>
+      <script>
+        $('#openclose').click(function(){
+          if($(this).text() == 'ABRIR') $(this).text('CERRAR');
+          else if ($(this).text() == 'CERRAR') $(this).text('ABRIR');
+        });
+      </script><!-- /script open/close -->
+    </div><!-- /div .list-group-item -->
   </div><!-- /div .list-group -->
 @endif
