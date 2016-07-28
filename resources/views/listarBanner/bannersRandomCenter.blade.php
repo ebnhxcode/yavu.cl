18ed13694bee75d4a182663e51e6189a8d6504ad
Esteban r <esteban.ramos.f@gmail.com>
2016-07-28 14:34:08 -0400

    update - correccion de los banner chicos que se ven al centro

--- resources/views/listarBanner/bannersRandomCenter.blade.php
+++ resources/views/listarBanner/bannersRandomCenter.blade.php
@@ -5,17 +5,22 @@
         <a href="/empresas/{{$bannersRandomCenter[0]->empresa_id}}">
           <img id="firstBannerCenter" src="/img/users/{!!($bannersRandomCenter[0]->banner!='')?$bannersRandomCenter[0]->banner:'banner.png'!!}" alt="..." style="height: 50px;border-radius: 5px;" >
         </a>
-        <a href="/empresas/{{$bannersRandomCenter[0]->empresa_id}}" class="btn btn-default btn-xs">
-          ver perfil
-        </a>
-        @if(count($bannersRandomCenter[0]->companyId->followers)>0)
-          <div class="btn-group" role="group" aria-label="...">
-            <a href="{!! URL::to('/empresas/'.$bannersRandomCenter[0]->empresa_id.'/sorteos') !!}" class="btn btn-success btn-xs">
-              <img src="{!! asset('img/yavu019.png') !!}" width="20" alt=""/>
-              Ver sorteos
-            </a>
-          </div><!-- /div .btn-group -->
-        @endif
+
+        <div class="softText-descriptions-middle" style="padding: 3px;">
+
+          <a href="/empresas/{!! $bannersRandomCenter[0]->empresa_id !!}">
+            <b>{!! $bannersRandomCenter[0]->companyName->nombre !!}</b>
+            <span class="softText-descriptions">
+              <small>{{$bcf = count($bannersRandomCenter[0]->companyId->followers)}} seguidor{{($bcf>2)?'es':''}}.</small>
+            </span>
+          </a>
+
+          <div class="softText-descriptions" style="padding-bottom: 5px;">
+            {{ $bannersRandomCenter[0]->descripcion_banner }}
+          </div>
+
+        </div><!-- /div .caption -->
+
       </div><!-- /col-xs12-sm12-md6-lg6 -->
     </div><!-- /div .row -->
   </div><!-- /div .list-group-item -->
@@ -24,15 +29,18 @@
 @if($key == 9)
 
   <div class="list-group-item">
-    <div class="row">
+    <div align="middle" class="row">
       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <img id="firstBannerCenter" src="/img/users/{!!($bannersRandomCenter[1]->banner!='')?$bannersRandomCenter[1]->banner:'banner.png'!!}" alt="..." style="height: 50px;">
+
+        <a href="/empresas/{{$bannersRandomCenter[1]->empresa_id}}">
+          <img id="firstBannerCenter" src="/img/users/{!!($bannersRandomCenter[1]->banner!='')?$bannersRandomCenter[1]->banner:'banner.png'!!}" alt="..." style="height: 50px;border-radius: 5px;" >
+        </a>
 
         <div class="softText-descriptions-middle" style="padding: 3px;">
 
           <a href="/empresas/{!! $bannersRandomCenter[1]->empresa_id !!}">
             <b>{!! $bannersRandomCenter[1]->companyName->nombre !!}</b>
-            <span class="softText-descriptions" style="float:right;">
+            <span class="softText-descriptions">
               <small>{{$bcf = count($bannersRandomCenter[1]->companyId->followers)}} seguidor{{($bcf>2)?'es':''}}.</small>
             </span>
           </a>
@@ -54,18 +62,26 @@
   <div class="list-group-item">
     <div align="middle" class="row">
       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <img id="firstBannerCenter" src="/img/users/{!!($bannersRandomCenter[2]->banner!='')?$bannersRandomCenter[2]->banner:'banner.png'!!}" alt="..." style="height: 50px;">
-        <a href="/empresas/{{$bannersRandomCenter[2]->empresa_id}}" class="btn btn-default btn-xs">
-          ver perfil
+
+        <a href="/empresas/{{$bannersRandomCenter[2]->empresa_id}}">
+          <img id="firstBannerCenter" src="/img/users/{!!($bannersRandomCenter[2]->banner!='')?$bannersRandomCenter[2]->banner:'banner.png'!!}" alt="..." style="height: 50px;border-radius: 5px;" >
         </a>
-        @if(count($bannersRandomCenter[2]->companyId->followers)>0)
-          <div class="btn-group" role="group" aria-label="...">
-            <a href="{!! URL::to('/empresas/'.$bannersRandomCenter[2]->empresa_id.'/sorteos') !!}" class="btn btn-success btn-xs">
-              <img src="{!! asset('img/yavu019.png') !!}" width="20" alt=""/>
-              Ver sorteos
-            </a>
-          </div><!-- /div .btn-group -->
-        @endif
+
+        <div class="softText-descriptions-middle" style="padding: 3px;">
+
+          <a href="/empresas/{!! $bannersRandomCenter[2]->empresa_id !!}">
+            <b>{!! $bannersRandomCenter[2]->companyName->nombre !!}</b>
+            <span class="softText-descriptions">
+              <small>{{$bcf = count($bannersRandomCenter[2]->companyId->followers)}} seguidor{{($bcf>2)?'es':''}}.</small>
+            </span>
+          </a>
+
+          <div class="softText-descriptions" style="padding-bottom: 5px;">
+            {{ $bannersRandomCenter[2]->descripcion_banner }}
+          </div>
+
+        </div><!-- /div .caption -->
+
       </div><!-- /col-xs12-sm12-md6-lg6 -->
     </div><!-- /div .row -->
   </div><!-- /div .list-group-item -->
