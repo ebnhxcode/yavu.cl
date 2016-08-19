e96131e4a1902c0aba158b38dccaa6e28ea78845
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-08-19 18:27:15 -0300

    hotfix - cambios a local

--- public/js/ajax/ParticiparSorteo.js
+++ public/js/ajax/ParticiparSorteo.js
@@ -67,7 +67,7 @@ $(document).ready(function(){
 
 /*FUNCIONES Y PROCEDIMIENTOS*/
 	function CargarDetallesSorteo(sorteo_id){
-		var route = "http://186.64.123.143/cargardetallessorteo/"+sorteo_id;
+		var route = "http://localhost:8000/cargardetallessorteo/"+sorteo_id;
 		$.ajax({
 			url: route,
 			type: 'GET',
@@ -110,7 +110,7 @@ $(document).ready(function(){
 	function ContarNotificaciones(){
 		var user_id = $("#user_id").val();
 		$.ajax({
-			url: "http://186.64.123.143/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
+			url: "http://localhost:8000/cargarpops/"+$("#idUltimaNotificacion").val()+"/"+user_id+"/novistas",
 			type: 'GET',
 			dataType: 'json',
 			cache: false,
@@ -132,7 +132,7 @@ $(document).ready(function(){
 	}
 	function MostrarGanador(Ganador){
 		//console.log("este es: "+Ganador);
-		var route = "http://186.64.123.143/mostrarganador/"+Ganador;
+		var route = "http://localhost:8000/mostrarganador/"+Ganador;
 		$.ajax({
 			url: route,
 			type: 'GET',
@@ -156,7 +156,7 @@ $(document).ready(function(){
 	function CanjearTicket(){
 		$('#myModal').modal('hide');
 		var user_id = $("#user_id").val();
-		var route = "http://186.64.123.143/canjearticket/"+user_id;
+		var route = "http://localhost:8000/canjearticket/"+user_id;
 		$.ajax({
 			url: route,
 			headers: {'X-CSRF-TOKEN': token},
@@ -188,7 +188,7 @@ $(document).ready(function(){
 			var CantidadActual = $(this).attr('value');
 			CantidadActual = CantidadActual | 0;
 			var CantidadTicketsUsados = 0;
-			var route = "http://186.64.123.143/contarticketsensorteo/"+$(this).attr('id');
+			var route = "http://localhost:8000/contarticketsensorteo/"+$(this).attr('id');
 			$.ajax({
 				url: route,
 				type: 'GET',
@@ -231,7 +231,7 @@ $(document).ready(function(){
 
 			var CantidadActual = $(this).attr('value');
 			CantidadActual = CantidadActual | 0;
-			var route = "http://186.64.123.143/contarticketsensorteo/"+$(this).attr('id');
+			var route = "http://localhost:8000/contarticketsensorteo/"+$(this).attr('id');
 			$.ajax({
 				url: route,
 				type: 'GET',
@@ -268,7 +268,7 @@ $(document).ready(function(){
 	}
 
 	function UsarTicket(sorteo_id){
-		var route = "http://186.64.123.143/usarticket/";
+		var route = "http://localhost:8000/usarticket/";
 		var token = $("#token").val();
 		$.ajax({
 			url: route,
@@ -299,7 +299,7 @@ $(document).ready(function(){
 	}
 
 	function UsarYavuCoins(sorteo_id){
-		var route = "http://186.64.123.143/usaryavucoins";
+		var route = "http://localhost:8000/usaryavucoins";
 		var token = $("#token").val();
 		$.ajax({
 			url: route,
@@ -331,7 +331,7 @@ $(document).ready(function(){
 
 
 	function RegistrarParticipanteGanador(Ganador){
-		var route = "http://186.64.123.143/registrarganadorsorteo/"+Ganador;
+		var route = "http://localhost:8000/registrarganadorsorteo/"+Ganador;
 		$.ajax({
 			url: route,
 			type: 'GET',
@@ -347,7 +347,7 @@ $(document).ready(function(){
 
 	function VerificarTickets(){
 		var user_id = $("#user_id").val();
-		var route = "http://186.64.123.143/verificartickets/"+user_id;
+		var route = "http://localhost:8000/verificartickets/"+user_id;
 		$.get(route, function(res){
 			if(parseInt(res)>0){
 				$(".UsarTicket").removeAttr('style');
@@ -362,7 +362,7 @@ $(document).ready(function(){
 
 	function VerificarYavuCoins(){
 		var user_id = $("#user_id").val();
-		var route = "http://186.64.123.143/verificaryavucoins";
+		var route = "http://localhost:8000/verificaryavucoins";
 		$.get(route, function(res){
 			if(parseInt(res)>0){
 				$(".UsarYavuCoins").removeAttr('style');
@@ -376,7 +376,7 @@ $(document).ready(function(){
 	}
 
 	function ContarCoins(){
-		var route = "http://186.64.123.143/contarcoins";
+		var route = "http://localhost:8000/contarcoins";
 		var user_id = $("#user_id");
 		$.get(route, function(res){
 			$(".CantidadCoins").text("");
@@ -390,7 +390,7 @@ $(document).ready(function(){
 	}
 
 	function ContarTickets(){
-		var route = "http://186.64.123.143/contartickets";
+		var route = "http://localhost:8000/contartickets";
 		var user_id = $("#user_id");
 		$.get(route, function(res){
 			$(".CantidadTickets").text(formatNumber.new(parseInt(res), "# "));
--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <div>
-<img src="http://186.64.123.143/img/yavu005.png" width="120" height="30" border="0" alt=""/>
+<img src="http://localhost:8000/img/yavu005.png" width="120" height="30" border="0" alt=""/>
 <hr size="4" color="#5c8c09">
 </div>
 <h2>¡Hola <strong>{!!$nombre!!}</strong>, bienvenido a la comunidad Yavü!</h2>
@@ -12,7 +12,7 @@
 <font style="font-family: Verdana, Geneva, sans-serif; color:#666766; font-size:13px; line-height:21px">
 En Yavu.cl podrás encontrar promociones, descuentos, concursos y eventos de los comercios de tu ciudad. Junta nuestras Yavü Coins en las publicaciones de las empresas, úsalas comprando tickets y utilízalos para participar de los sorteos!
 <br>
-<p><strong>Para validar tu cuenta haz click en el enlace de validación: </strong><a href="http://186.64.123.143/verificarusuario/{!! $codigo !!}"> Abrir </a>  </p><br>
+<p><strong>Para validar tu cuenta haz click en el enlace de validación: </strong><a href="http://localhost:8000/verificarusuario/{!! $codigo !!}"> Abrir </a>  </p><br>
 ¡Mucha suerte! y nos vemos en Yavü.
 </div>
 <hr size="4" color="#5c8c09">
@@ -20,13 +20,13 @@ En Yavu.cl podrás encontrar promociones, descuentos, concursos y eventos de los
 	<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
 	  <tr>
 	    <td width="13%" align="center">&nbsp;</td>
-	    <td width="14%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://186.64.123.143/contacto" style="color:#010203; text-decoration:none"><strong>UNSUBSCRIBE </strong></a></font></td>
+	    <td width="14%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://localhost:8000/contacto" style="color:#010203; text-decoration:none"><strong>UNSUBSCRIBE </strong></a></font></td>
 	    <td width="2%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>|</strong></font></td>
-	    <td width="9%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://186.64.123.143/nosotros" style="color:#010203; text-decoration:none"><strong>ABOUT </strong></a></font></td>
+	    <td width="9%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://localhost:8000/nosotros" style="color:#010203; text-decoration:none"><strong>ABOUT </strong></a></font></td>
 	    <td width="2%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>|</strong></font></td>
-	    <td width="11%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://186.64.123.143/contacto" style="color:#010203; text-decoration:none"><strong>CONTACTO </strong></a></font></td>
+	    <td width="11%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://localhost:8000/contacto" style="color:#010203; text-decoration:none"><strong>CONTACTO </strong></a></font></td>
 	    <td width="2%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><strong>|</strong></font></td>
-	    <td width="17%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://186.64.123.143" style="color:#010203; text-decoration:none"><strong>SOCIAL</strong></a></font></td>
+	    <td width="17%" align="center"><font style="font-family:'Myriad Pro', Helvetica, Arial, sans-serif; color:#010203; font-size:9px; text-transform:uppercase"><a href= "http://localhost:8000" style="color:#010203; text-decoration:none"><strong>SOCIAL</strong></a></font></td>
 	    <td width="4%" align="right"><a href="https://www.facebook.com/yavu.cl" target="_blank"><img src="https://www.facebookbrand.com/img/assets/asset.f.logo.lg.png" alt="facebook" width="23" height="19" border="0" /></a></td>
 	    <td width="5%">&nbsp;</td>
 	  </tr>
