f41d4986de28c0dcde3f8598d582b4399f405589
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-08-30 06:44:55 -0300

    add - se agrego a la lista de categorias un maletin que indica que la empresa personal está categorizada con esa categoria.

--- resources/views/empresas/companyStatisticsPartial/statisticsTypesPartial/followersStatisticsPartial/tabPanes.blade.php
+++ resources/views/empresas/companyStatisticsPartial/statisticsTypesPartial/followersStatisticsPartial/tabPanes.blade.php
@@ -17,6 +17,10 @@
             <div class="list-group-item">
               <b>
                 {{$category->category}}
+                @if(count($category->belongs_to_this_company($userCompany->id))>0)
+                  <img style="float:right;" width="16" src="{{url('/img/glyphicons/glyphicons/png/glyphicons-342-briefcase.png')}}" data-toggle="tooltip" data-placement="top" title="Categor&iacute;a de mi empresa" style alt="">
+                @endif
+
               </b>
               <br>
               <div class="softText-descriptions-middle">
@@ -67,17 +71,32 @@
         <div align="center">
           <img width="16" src="{{url('/img/glyphicons/glyphicons/png/glyphicons-342-briefcase.png')}}" alt=""> Categor&iacute;as de mi empresa
         </div>
-        <br>
+        <hr>
 
         @foreach($userCompany->myCategories as $key => $companyCategoryObject)
           <div style="padding-bottom: 10px;" class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <div class="list-group-item">
               <!-- $cco -> $companyCategoryObject -->
-              <b>{{($cco = $companyCategoryObject->getCategory)?$cco->category:''  }}</b>
+              <b>{{($cco = $companyCategoryObject->getCategory)?$cco->category:''}}</b>
+
+
               <br>
 
               <div class="softText-descriptions">
-                {{ count($cco->userInteresteds) }}
+
+                <?php $interesteds=0; ?>
+                @foreach($followers as $key => $follower)
+                  <?php ( count($follower->interestedIn($cco->id))>0?$interesteds++:0 ) ?>
+                @endforeach
+                {{($interesteds)>0?$interesteds.' de tus seguidores tienen intereses en esta categor&iacute;a':'no tienes seguidores interesados en esta categor&iacute;a'}}
+                <br>
+
+                <span>
+                  <small class="softText-descriptions" style="float:right;">
+                    {{-- ver m&aacute;s --}}
+                  </small><!-- /small .softText-descriptions -->
+                </span>
+
 
                 <br>
                 +------------------------+ <br>
