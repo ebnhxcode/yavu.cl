a1acde26837f1e05335a6157c90895cbeff78338
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-09-01 19:39:27 -0300

    update - backup, aplicando diseño de grafico de visitas a seguidores

--- resources/views/empresas/companyStatisticsPartial/statisticsTypesPartial/visitsStatisticsPartial/graphics.blade.php
+++ resources/views/empresas/companyStatisticsPartial/statisticsTypesPartial/visitsStatisticsPartial/graphics.blade.php
@@ -1,18 +1,20 @@
+<!-- ################################ -->
+<!--
+        DETALLE DE VARIABLES QUE TENGO PARA DESPLEGAR EN CADA GRÁFICO :
+        Cada una es un objeto arreglo con la cantidad asociada a la relación
+        $totalVisits;$menVisits;$womenVisits;$otherVisits;$userCompany;$userSession;$category;$graphicType;
+
+--!>
+
+<!-- ################################ -->
+
+
+
+
 <div id="Graphic{!! $category->id !!}Category{{$graphicType}}" class="collapse">
   +---------------------------------------------------------------------------------------+ <br>
   | <br>
   |  insertar gráfico <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
-  | <br>
   | <br>
   | <br>
   | <br>
@@ -23,7 +25,7 @@
 </div><!-- #GraphicsIdCategory .collapse -->
 
 <!-- boton que cierra y abre la caja del gráfico -->
-<small class="btn btn-xs btn-default">
+<small class="btn btn-sm btn-default">
   <span class="openclose" data-toggle="collapse" data-target="#Graphic{{$category->id}}Category{{$graphicType}}" style="float:right;">
     ver más detalles
   </span><!-- #openclose .btn .btn-default .btn-xs -->
