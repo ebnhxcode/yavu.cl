41ebbf5e6f37217bd949f6b4682c56393ab8cfbe
Esteban Ramos <esteban.ramos.f@gmail.com>
2016-09-30 16:50:25 -0300

    hotfix - cambios a local

--- resources/views/empresas/forms/fieldsEmpresa.blade.php
+++ resources/views/empresas/forms/fieldsEmpresa.blade.php
@@ -379,7 +379,7 @@
 	});
 
 	function ValidarRut(rut){
-		var route = "http://186.64.123.143/validarrutempresa/"+rut+"";
+		var route = "http://yavu.local/validarrutempresa/"+rut+"";
 		document.getElementById('FormEmpresa').onsubmit = function() {
 		    return false;
 		}
